4edea28a0e5185dd5bf909f368835f78df07b443
Christopher Dignam <chris@dignam.xyz>
2017-06-12 22:25:45 -0400

    Cleaned up 404 page
    - Added auto-redirect
    - Fixed layout

--- frontend/src/NoMatch.js
+++ frontend/src/NoMatch.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const NoMatch = () => (
-  <div>
-    <nav className="grid container">
-      <h1 className="col-xs-2">
-        <Link to="/" className="nav-item">Caena</Link>
-      </h1>
-    </nav>
+class NoMatch extends React.Component {
+  componentDidMount () {
+    setTimeout(() => { this.props.history.push('/') }, 5000)
+  }
+  render () {
+    return (
+      <div>
+        <nav className="grid container">
+          <h1 className="col-xs-2">
+            <Link to="/" className="nav-item">Caena</Link>
+          </h1>
+        </nav>
 
-    <main className="grid container">
-      <section className="grid-center">
-        <h2>404</h2>
-        <p>Page not found.</p>
-        <p>Go <Link to="/">Home</Link></p>
-      </section>
-    </main>
-
-  </div>
-)
+        <main className="grid container grid-center">
+          <section className="col-md-12 col-xs-12">
+            <h2>404 – File not found.</h2>
+            <p>You will be automatically redirect <Link to="/">home</Link> in five seconds.</p>
+          </section>
+        </main>
+      </div>
+    )
+  }
+}
 
 export default NoMatch
