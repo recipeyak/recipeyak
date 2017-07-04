cec85a82e29243000f99ff557eb244dc2db4a0e7
Steve Dignam <steve@dignam.xyz>
2017-07-03 22:34:34 -0400

    Style and formatting: using more destructuring
    Also fixing indenting and misc. style choices

--- frontend/src/RecipeList.jsx
+++ frontend/src/RecipeList.jsx
@@ -8,19 +8,18 @@ import Recipe from './RecipeItem.jsx'
 import 'bulma/css/bulma.css'
 
 const RecipeList = ({ recipes = [], removeFromCart, addToCart }) => {
-  let recipeList = recipes.map(recipe =>
-    <div className="grid-item" key={ recipe.id }>
-      <Recipe
-        {...recipe}
-        key={ recipe.id }
-        removeFromCart={ () => removeFromCart(recipe.id)}
-        addToCart={ () => addToCart(recipe.id)}
-      />
-    </div>
-  )
-  if (recipes.length === 0) {
-    recipeList = <div className="grid-item">No Recipes</div>
-  }
+  const recipeList = recipes.length !== 0
+    ? recipes.map(recipe =>
+        <div className="grid-item" key={ recipe.id }>
+          <Recipe
+            {...recipe}
+            key={ recipe.id }
+            removeFromCart={ () => removeFromCart(recipe.id)}
+            addToCart={ () => addToCart(recipe.id)}
+          />
+        </div>)
+    : <div className="grid-item">No Recipes</div>
+
   return (
     <div className="container">
       <Navbar/>
