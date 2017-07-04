37d0933246ffc0eebec4881f2878d5ec279baca3
Steve Dignam <steve@dignam.xyz>
2017-07-04 12:06:54 -0400

    Minor: removed auto redirect timer for 404 page

--- frontend/src/NoMatch.jsx
+++ frontend/src/NoMatch.jsx
@@ -1,28 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-class NoMatch extends React.Component {
-  componentDidMount () {
-    setTimeout(() => { this.props.history.push('/') }, 5000)
-  }
-  render () {
-    return (
-      <div>
-        <nav className="grid container">
-          <h1 className="col-xs-2">
-            <Link to="/" className="nav-item">Caena</Link>
-          </h1>
-        </nav>
+const NoMatch = () => (
+  <div>
+    <nav className="grid container">
+      <h1 className="col-xs-2">
+        <Link to="/" className="nav-item">Caena</Link>
+      </h1>
+    </nav>
 
-        <main className="grid container grid-center">
-          <section className="col-md-12 col-xs-12">
-            <h2>404 – File not found.</h2>
-            <p>You will be automatically redirect <Link to="/">home</Link> in five seconds.</p>
-          </section>
-        </main>
-      </div>
-    )
-  }
-}
+    <main className="grid container grid-center">
+      <section className="col-md-12 col-xs-12">
+        <h2>404 – File not found.</h2>
+        <p>Head <Link to="/">home</Link></p>
+      </section>
+    </main>
+  </div>
+)
 
 export default NoMatch
