7b5f1e21ba361c097c255067d40462ba5fd9fd4f
Christopher Dignam <chris@dignam.xyz>
2017-08-06 09:39:44 -0400

    Updated models

--- backend/core/models.py
+++ backend/core/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
-class UserManager(BaseUserManager):
+class MyUserManager(BaseUserManager):
     def create_user(self, email, password=None):
+        """
+        Creates and saves a user with given email and password.
+        """
         if not email:
             raise ValueError('User must have an email address')
         user = self.model(email=self.normalize_email(email))
@@ -11,21 +14,22 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    # https://docs.djangoproject.com/en/1.10/topics/auth/customizing/#django.contrib.auth.models.CustomUserManager.create_superuser
     def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
         user = self.create_user(email=email, password=password)
         user.is_admin = True
-        user.is_superuser = True
-        user.is_active = True
         user.save(using=self._db)
         return user
 
 class MyUser(AbstractBaseUser):
+    """Custom user model that only requires an email and password"""
     email = models.EmailField(unique=True)
 
+    # required for admin
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
 
     created = models.DateField(auto_now_add=True)
     last_updated = models.DateField(auto_now=True)
@@ -33,7 +37,7 @@ class MyUser(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    objects = UserManager()
+    objects = MyUserManager()
 
     def get_full_name(self):
         return self.email
@@ -41,12 +45,19 @@ class MyUser(AbstractBaseUser):
     def get_short_name(self):
         return self.email
 
+    # required for admin
     def has_perm(self, perm, obj=None):
+        """Does the user have a specific permission?"""
+        # TODO: Add permissions
         return True
 
+    # required for admin
     def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app `app_label`?"""
+        # TODO: Add permissions
         return True
 
+    # required for admin
     @property
     def is_staff(self):
         return self.is_admin
