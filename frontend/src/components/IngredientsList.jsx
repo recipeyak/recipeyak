ca9b8f81c9a58f2953ef0af4e98c3453fc3741a6
Steve Dignam <steve@dignam.xyz>
2017-09-07 16:01:18 -0400

    Moved: helper functions into respective file
    I think more could be done to make use of lodash
    
    - moved tests into component relevant test file

--- frontend/src/components/IngredientsList.jsx
+++ frontend/src/components/IngredientsList.jsx
@@ -1,7 +1,49 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { combineRecipeIngredients } from '../helpers.js'
+const flatten = arr => arr.reduce(
+  (acc, val) => acc.concat(
+    Array.isArray(val) ? flatten(val) : val
+  ),
+  []
+)
+
+// flatten function used inside reduce functions
+const reduceFlatten = (acc, val) =>
+  acc.concat(
+    Array.isArray(val)
+    ? flatten(val)
+    : val
+  )
+
+const countDupes = (acc, b) => {
+  if (acc[b] == null) {
+    acc[b] = 1
+  } else {
+    acc[b]++
+  }
+  return acc
+}
+
+const expandOccurances = rec => {
+  const occurs = rec.inCart
+  const output = []
+  for (let i = 0; i < occurs; i++) {
+    output.push(rec.ingredients)
+  }
+  return output
+}
+
+export const combineRecipeIngredients = (recipes) => {
+  const recipesObject = recipes
+    .map(expandOccurances)
+    .reduce(reduceFlatten, [])
+    .reduce(countDupes, {})
+
+  return Object.keys(recipesObject)
+    .map(key => ({ ingredient: key, occurs: recipesObject[key] }))
+    .sort((a, b) => a.ingredient > b.ingredient)
+}
 
 const IngredientsList = ({ recipes = [] }) => {
   const ingredients =
