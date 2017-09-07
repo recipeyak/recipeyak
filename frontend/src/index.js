8d4fcfecd6ec904f2093e33d75c07d86221900ff
Steve Dignam <steve@dignam.xyz>
2017-09-07 00:20:18 -0400

    Added: react hot module reloading

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 
 import App from './components/App.jsx'
@@ -9,14 +10,29 @@ import store from './store/store.js'
 import './grid.scss'
 
 const rootElement = document.getElementById('root')
+if (rootElement == null) {
+  throw new Error('could not find root element')
+}
 
-if (rootElement != null) {
-  render(
+render(
+  <AppContainer>
     <Provider store={ store }>
       <App />
-    </Provider>,
-    rootElement
-  )
-} else {
-  console.error('error finding root element')
+    </Provider>
+  </AppContainer>,
+  rootElement
+)
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default
+    render(
+      <AppContainer>
+        <Provider store={ store }>
+          <NextApp/>
+        </Provider>
+      </AppContainer>,
+      rootElement
+    )
+  })
 }
