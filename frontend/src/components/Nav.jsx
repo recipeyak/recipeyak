fb000031fff4103af227fdce1dbdf1240cc1bf94
Steve Dignam <steve@dignam.xyz>
2017-10-06 18:53:26 -0400

    Removed: searchbox from navbar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
-import SearchBox from '../containers/SearchBox.jsx'
-
 import './nav.scss'
 
 class Navbar extends React.Component {
@@ -11,39 +9,16 @@ class Navbar extends React.Component {
     this.state = {
       query: '',
       showNav: false,
-      showSearchResults: false,
       showDropdown: false,
     }
-
-    document.addEventListener('mousedown', (e) => this.handleSearchBoxClick(e))
   }
 
-  handleSearchBoxClick (e) {
-    const el = this.refs.search
-    // this is usually null when the search box is not rendered (active)
-    if (el == null) return
-
-    const clickOnSearch = el.contains(e.srcElement)
-    if (!clickOnSearch) {
-      this.setState({ showSearchResults: false })
-    }
+  toggleNav = () => {
+    this.setState(prevState => ({ showNav: !prevState.showNav }))
   }
 
-  componentWillUnmount () {
-    document.removeEventListener('mousedown', (e) => this.handleSearchBoxClick(e))
-  }
-
-  toggleNav () {
-    this.setState((prevState, props) => ({ showNav: !prevState.showNav }))
-  }
-
-  toggleDropdown () {
-    this.setState((prevState, props) => ({ showDropdown: !prevState.showDropdown }))
-  }
-
-  handleQueryChange (e) {
-    const value = e.target.value
-    this.setState({ query: value })
+  toggleDropdown = () => {
+    this.setState(prevState => ({ showDropdown: !prevState.showDropdown }))
   }
 
   logout = () => {
@@ -58,16 +33,8 @@ class Navbar extends React.Component {
           <Link to="/" className="title">Recipe Yak</Link>
         </div>
       </div>
-      <div ref="search" className="nav-center align-center">
-        <SearchBox
-          showSearchResults={ this.state.showSearchResults }
-          handleOnFocus={ () => this.setState({ showSearchResults: true }) }
-          handleQueryChange={ (e) => this.handleQueryChange(e) }
-          query={ this.state.query }
-        />
-      </div>
 
-      <span onClick={ () => this.toggleNav() } className={ 'nav-toggle' + (this.state.showNav ? ' is-active' : '') }>
+      <span onClick={ this.toggleNav } className={ 'nav-toggle' + (this.state.showNav ? ' is-active' : '') }>
         <span></span>
         <span></span>
         <span></span>
@@ -83,7 +50,7 @@ class Navbar extends React.Component {
         </div>
         <NavLink activeClassName="is-active" to="/recipes" className="nav-item">Recipes</NavLink>
         <NavLink activeClassName="is-active" to="/cart" className="nav-item">Cart</NavLink>
-        <div onClick={() => this.toggleDropdown() } className="nav-item user-profile">
+        <div onClick={ this.toggleDropdown } className="nav-item user-profile">
           <img alt="user profile" className="user-profile-image" src="https://www.gravatar.com/avatar/ea7be1e5200ad6934add15a721b5b1b0?d=identicon"/>
           <svg aria-hidden="true" height="11" version="1.1" viewBox="0 0 12 16" width="8">
             <path d="M0 5l6 6 6-6z"></path>
