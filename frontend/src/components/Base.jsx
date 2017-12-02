bbbf7b478380876c52436106ef6312272acafbbc
Steve Dignam <steve@dignam.xyz>
2017-12-02 13:27:19 -0500

    Fix: alignment of navbar items at small width
    Now they center when the viewport is small

--- frontend/src/components/Base.jsx
+++ frontend/src/components/Base.jsx
@@ -5,7 +5,7 @@ import Navbar from '../containers/Nav.jsx'
 const Base = ({ children }) => (
   <div className="container pl-4 pr-4">
     <Navbar />
-    <div className="pb-4 pt-2">
+    <div className="pb-4 pt-0">
       { children }
     </div>
   </div>
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -35,7 +35,7 @@ class Navbar extends React.Component {
     )
 
     return (
-      <nav className="nav justify-space-between flex-wrap">
+      <nav className="nav flex-wrap pt-2 pb-2">
         <Link to="/" className="better-nav-item pl-0 pr-0 fs-2rem fw-normal font-family-serif">Recipe Yak</Link>
         { buttons }
       </nav>
