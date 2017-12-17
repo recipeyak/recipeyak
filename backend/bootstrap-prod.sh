1c7ffae60e00b307c642d22387f4696936c4b10e
Christopher Dignam <chris@dignam.xyz>
2017-12-16 23:07:33 -0500

    Fix django logging to stdout (#85)

--- backend/bootstrap-prod.sh
+++ backend/bootstrap-prod.sh
@@ -5,4 +5,4 @@ done
 python manage.py collectstatic --noinput
 python manage.py migrate
 ./manage.py shell < set_server.py
-gunicorn -w 3 -b 0.0.0.0:8000 backend.wsgi --access-logfile - --error-logfile - --capture-output
+PYTHONUNBUFFERED=1 gunicorn -w 3 -b 0.0.0.0:8000 backend.wsgi --access-logfile - --error-logfile - --capture-output --enable-stdio-inheritance
