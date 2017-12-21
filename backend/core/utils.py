1324abe29b7590d71e32cf8d9a176c2e68074158
Steve Dignam <steve@dignam.xyz>
2017-12-21 18:29:21 -0500

    Refactor: combine_ingredients to be more readable

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -8,25 +8,23 @@ ureg = UnitRegistry()
 
 def combine_ingredients(ingredients: List) -> List:
 
-    combined: Dict[str, Dict[str, Quantity]] = {
-        # 'name': {
-        #     'base unit': <Quantity 1.234, 'tablespoon'>,
-        # }
-    }
+    combined: Dict[str, Dict[str, Quantity]] = {}
 
     for ingredient in ingredients:
         try:
             quantity = ureg.parse_expression(ingredient.quantity)
         except UndefinedUnitError:
             quantity = ingredient.quantity
+
         name = ingredient.name
+
         try:
             base_unit = quantity.to_base_units().units
         except AttributeError:
             base_unit = quantity
 
-        if combined.get(name, None) is not None:
-
+        in_combined = combined.get(name, None) is not None
+        if in_combined:
             if combined.get(name).get(base_unit, None) is not None:
                 combined[name][base_unit] += quantity
             else:
@@ -38,7 +36,10 @@ def combine_ingredients(ingredients: List) -> List:
 
     simple_ingredients = []
     for name, v in combined.items():
-        for unit in v.values():
-            simple_ingredients.append({'unit': str(unit), 'name': name})
+        unit, = v.values()
+        simple_ingredients.append({
+            'unit': str(unit),
+            'name': name
+        })
 
     return simple_ingredients
