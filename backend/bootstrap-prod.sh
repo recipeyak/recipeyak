86f0e5ca62c46aa2a921de626dbe4a0c591ecd02
Christopher Dignam <chris@dignam.xyz>
2018-01-03 15:52:00 -0500

    Simplify cp commands

--- backend/bootstrap-prod.sh
+++ backend/bootstrap-prod.sh
@@ -1,5 +1,5 @@
 # copy static files to mounted volume (app/static => app/static-files)
-cd static && cp -a . ../static-files && cd ..
+cp -a static/* static-files
 # wait for database
 while ! nc -w 1 -z db 5432
     do sleep 0.1
--- frontend/bootstrap.sh
+++ frontend/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Copy files from build/ (our current WORKDIR) to the volume mounted at dist
-cd build && cp -a . ../dist
+cp -a build/* dist
 tail -f /dev/null
