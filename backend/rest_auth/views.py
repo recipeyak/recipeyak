8ac79eab1e8d9c51c92aaeb59253fd64b30514d0
Steve Dignam <steve@dignam.xyz>
2018-01-03 23:27:22 -0500

    Simplified login and logout views

--- backend/rest_auth/views.py
+++ backend/rest_auth/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import (
-    login as django_login,
-    logout as django_logout
+    login,
+    logout
 )
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -18,10 +18,9 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from .app_settings import (
     TokenSerializer, UserDetailsSerializer, LoginSerializer,
     PasswordResetSerializer, PasswordResetConfirmSerializer,
-    PasswordChangeSerializer, JWTSerializer, create_token
+    PasswordChangeSerializer, create_token
 )
 from .models import TokenModel
-from .utils import jwt_encode
 
 sensitive_post_parameters_m = method_decorator(
     sensitive_post_parameters(
@@ -41,59 +40,29 @@ class LoginView(GenericAPIView):
     Return the REST Framework Token Object's key.
     """
     permission_classes = (AllowAny,)
-    serializer_class = LoginSerializer
-    token_model = TokenModel
 
     @sensitive_post_parameters_m
     def dispatch(self, *args, **kwargs):
         return super(LoginView, self).dispatch(*args, **kwargs)
 
-    def process_login(self):
-        django_login(self.request, self.user)
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(
+            data=request.data,
+            context={'request': request})
 
-    def get_response_serializer(self):
-        if getattr(settings, 'REST_USE_JWT', False):
-            response_serializer = JWTSerializer
-        else:
-            response_serializer = TokenSerializer
-        return response_serializer
+        serializer.is_valid(raise_exception=True)
 
-    def login(self):
-        self.user = self.serializer.validated_data['user']
+        user = serializer.validated_data.get('user')
 
-        if getattr(settings, 'REST_USE_JWT', False):
-            self.token = jwt_encode(self.user)
-        else:
-            self.token = create_token(self.token_model, self.user,
-                                      self.serializer)
+        token = create_token(TokenModel, user, serializer)
 
-        if getattr(settings, 'REST_SESSION_LOGIN', True):
-            self.process_login()
+        login(request, user)
 
-    def get_response(self):
-        serializer_class = self.get_response_serializer()
+        serializer = TokenSerializer(instance=token, context={'request': request})
 
-        if getattr(settings, 'REST_USE_JWT', False):
-            data = {
-                'user': self.user,
-                'token': self.token
-            }
-            serializer = serializer_class(instance=data,
-                                          context={'request': self.request})
-        else:
-            serializer = serializer_class(instance=self.token,
-                                          context={'request': self.request})
+        # TODO: return user
 
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request, *args, **kwargs):
-        self.request = request
-        self.serializer = self.get_serializer(data=self.request.data,
-                                              context={'request': request})
-        self.serializer.is_valid(raise_exception=True)
-
-        self.login()
-        return self.get_response()
 
 
 class LogoutView(APIView):
@@ -105,24 +74,13 @@ class LogoutView(APIView):
     """
     permission_classes = (AllowAny,)
 
-    def get(self, request, *args, **kwargs):
-        if getattr(settings, 'ACCOUNT_LOGOUT_ON_GET', False):
-            response = self.logout(request)
-        else:
-            response = self.http_method_not_allowed(request, *args, **kwargs)
-
-        return self.finalize_response(request, response, *args, **kwargs)
-
     def post(self, request, *args, **kwargs):
-        return self.logout(request)
-
-    def logout(self, request):
         try:
             request.user.auth_token.delete()
         except (AttributeError, ObjectDoesNotExist):
             pass
 
-        django_logout(request)
+        logout(request)
 
         return Response({"detail": _("Successfully logged out.")},
                         status=status.HTTP_200_OK)
