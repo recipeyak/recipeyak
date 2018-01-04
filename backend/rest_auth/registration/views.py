576f78fb1884927f024ab47717eabc72f33ad637
Steve Dignam <steve@dignam.xyz>
2018-01-04 13:20:41 -0500

    Setup: django knox for views

--- backend/rest_auth/registration/views.py
+++ backend/rest_auth/registration/views.py
@@ -14,6 +14,8 @@ from allauth.account.views import ConfirmEmailView
 from allauth.account.utils import complete_signup
 from allauth.account import app_settings as allauth_settings
 
+from knox.models import AuthToken
+
 from rest_auth.app_settings import (TokenSerializer,
                                     JWTSerializer,
                                     create_token)
@@ -38,41 +40,29 @@ class RegisterView(CreateAPIView):
     def dispatch(self, *args, **kwargs):
         return super(RegisterView, self).dispatch(*args, **kwargs)
 
-    def get_response_data(self, user):
-        if allauth_settings.EMAIL_VERIFICATION == \
-                allauth_settings.EmailVerificationMethod.MANDATORY:
-            return {"detail": _("Verification e-mail sent.")}
-
-        if getattr(settings, 'REST_USE_JWT', False):
-            data = {
-                'user': user,
-                'token': self.token
-            }
-            return JWTSerializer(data).data
-        else:
-            return TokenSerializer(user.auth_token).data
-
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
 
-        return Response(self.get_response_data(user),
-                        status=status.HTTP_201_CREATED,
-                        headers=headers)
-
-    def perform_create(self, serializer):
         user = serializer.save(self.request)
-        if getattr(settings, 'REST_USE_JWT', False):
-            self.token = jwt_encode(user)
-        else:
-            create_token(self.token_model, user, serializer)
+
+        token = AuthToken.objects.create(user)
 
         complete_signup(self.request._request, user,
                         allauth_settings.EMAIL_VERIFICATION,
                         None)
-        return user
+
+        if allauth_settings.EMAIL_VERIFICATION == \
+                allauth_settings.EmailVerificationMethod.MANDATORY:
+            data = {"detail": _("Verification e-mail sent.")}
+        else:
+            data = {'key': token}
+
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(data,
+                        status=status.HTTP_201_CREATED,
+                        headers=headers)
 
 
 class VerifyEmailView(APIView, ConfirmEmailView):
