7e584baf45648756dcda85d17a3fbb9137a2078a
Steve Dignam <steve@dignam.xyz>
2018-01-05 11:34:59 -0500

    Added: test for django-rest-knox multi token setup

--- backend/core/test_authentication.py
+++ backend/core/test_authentication.py
@@ -91,3 +91,50 @@ def test_logout(client):
     res = client.get(f'{BASE_URL}/rest-auth/user/', HTTP_AUTHORIZATION='Token ' + key)
     assert res.status_code == status.HTTP_401_UNAUTHORIZED, \
         'logged out user was able to access login required info'
+
+
+def test_login_in_two_places_and_logout_from_one(client):
+    """
+    ensure when logged into one place, logging out doesn't result in logging out of both places.
+
+    This test ensure that the added functionality from django-rest-knox works
+    """
+    email = 'john@doe.org'
+    password = 'testing123'
+    user = MyUser.objects.create(email=email)
+    user.set_password(password)
+    user.save()
+
+    data = {
+        'email': email,
+        'password': password,
+    }
+
+    # 1. log in once
+    res = client.post(f'{BASE_URL}/rest-auth/login/', data)
+    assert res.status_code == status.HTTP_200_OK
+
+    first_login_key = res.json().get('key')
+
+    assert res.json().get('user') == UserSerializer(user).data
+
+    # 2. log in a second time
+    res = client.post(f'{BASE_URL}/rest-auth/login/', data)
+    assert res.status_code == status.HTTP_200_OK
+    second_login_key = res.json().get('key')
+
+    assert second_login_key != first_login_key
+
+    assert res.json().get('user') == UserSerializer(user).data
+
+    # 3. logout first login session
+    res = client.post(f'{BASE_URL}/rest-auth/logout/', HTTP_AUTHORIZATION='Token ' + first_login_key)
+    assert res.status_code == status.HTTP_200_OK
+
+    # 4. ensure first login key doesn't work
+    res = client.get(f'{BASE_URL}/rest-auth/user/', HTTP_AUTHORIZATION='Token ' + first_login_key)
+    assert res.status_code == status.HTTP_401_UNAUTHORIZED
+
+    # 4. ensure second login key still works
+    res = client.get(f'{BASE_URL}/rest-auth/user/', HTTP_AUTHORIZATION='Token ' + second_login_key)
+    assert res.status_code == status.HTTP_200_OK
