53d76aac190dd613630eecc697942c6a39454fcd
Christopher Dignam <chris@dignam.xyz>
2018-01-05 12:49:36 -0500

    Fix running tests in docker container (#17)
    * WIP
    
    * Fix running tests in docker container

--- frontend/scripts/test.js
+++ frontend/scripts/test.js
@@ -12,7 +12,7 @@ const argv = process.argv.slice(2)
 
 // Watch unless on CI or in coverage mode
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
-  argv.push('--watch')
+  argv.push('--watchAll')
 }
 
 jest.run(argv)
