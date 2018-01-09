3488376563ff6b48f7008c2eeeee7e3ac12050c2
Christopher Dignam <chris@dignam.xyz>
2018-01-08 20:54:19 -0500

    Add hidden-source-map

--- frontend/config/webpack.config.prod.js
+++ frontend/config/webpack.config.prod.js
@@ -46,7 +46,7 @@ module.exports = {
   bail: true,
   // We generate sourcemaps in production. This is slow but gives good results.
   // You can exclude the *.map files from the build during deployment.
-  devtool: 'source-map',
+  devtool: 'hidden-source-map',
   // In production, we only want to load the polyfills and the app code.
   entry: [
     require.resolve('./polyfills'),
