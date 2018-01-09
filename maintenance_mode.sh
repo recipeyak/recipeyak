4bf53d31ace0d5bcb5458ba94bec44980c45c308
Christopher Dignam <chris@dignam.xyz>
2018-01-09 15:22:17 -0500

    Fix typo

--- maintenance_mode.sh
+++ maintenance_mode.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 case $1 in
     on|ON)
-        printf "Turning off maintenance mode\n"
+        printf "Enabling maintenance mode\n"
         docker-compose -f docker-compose-prod.yml exec nginx touch maintenance_on
         printf "Maintenance mode: ON\n";;
     off|OFF)
-        printf "Turning off maintenance mode\n"
+        printf "Disabling maintenance mode\n"
         docker-compose -f docker-compose-prod.yml exec nginx rm -f maintenance_on
         printf "Maintenance mode: OFF\n";;
     *) printf "Invalid option. Use arguments 'on' or 'off'.\n";;
