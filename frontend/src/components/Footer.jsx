47fe3f702a631ddb6be6c35eff2bddda3d068d64
Steve Dignam <steve@dignam.xyz>
2018-01-29 11:19:59 -0500

    Added: footer with link to src everywhere

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () =>
+  <footer className="fw-bold pt-4 pb-4 fs-4 font-family-title d-flex space-between container pr-4 pl-4">
+    <span>Recipe Yak • <a href="https://github.com/recipeyak/recipeyak">src</a></span>
+    <span>Est. 2017</span>
+  </footer>
+
+export default Footer
