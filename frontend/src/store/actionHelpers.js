4603520fbb688007af041a07a4c8f34a44dcbab5
Steve Dignam <steve@dignam.xyz>
2018-02-18 00:04:38 -0500

    Removed: unused graph and action helper files

--- frontend/src/store/actionHelpers.js
+++
@@ -1,7 +0,0 @@
-export const makeActionCreator = (type, ...argNames) => (...args) => {
-  const action = { type }
-  argNames.forEach((_, index) => {
-    action[argNames[index]] = args[index]
-  })
-  return action
-}
