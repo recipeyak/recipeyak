9bf5ce4244f1a6b5b73eee9482adba52c34d4e1b
Christopher Dignam <chris@dignam.xyz>
2018-02-26 21:19:52 -0500

    Cleanup deployment.
    - Use hub.docker.come/recipeyak for container storage
    - Add tagging to git sha to images
    - Improve build, upload, and deploy scripts

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,32 +1,32 @@
 /* eslint no-unused-vars: 0 */
 
-const OAUTH_BITBUCKET_CLIENT_ID = process.env.OAUTH_BITBUCKET_CLIENT_ID
-const OAUTH_BITBUCKET_REDIRECT_URI = process.env.OAUTH_BITBUCKET_REDIRECT_URI
-
-const OAUTH_FACEBOOK_CLIENT_ID = process.env.OAUTH_FACEBOOK_CLIENT_ID
-const OAUTH_FACEBOOK_REDIRECT_URI = process.env.OAUTH_FACEBOOK_REDIRECT_URI
-
-const OAUTH_GITHUB_CLIENT_ID = process.env.OAUTH_GITHUB_CLIENT_ID
-const OAUTH_GITHUB_REDIRECT_URI = process.env.OAUTH_GITHUB_REDIRECT_URI
-
-const OAUTH_GITLAB_CLIENT_ID = process.env.OAUTH_GITLAB_CLIENT_ID
-const OAUTH_GITLAB_REDIRECT_URI = process.env.OAUTH_GITLAB_REDIRECT_URI
-
-const OAUTH_GOOGLE_CLIENT_ID = process.env.OAUTH_GOOGLE_CLIENT_ID
-const OAUTH_GOOGLE_REDIRECT_URI = process.env.OAUTH_GOOGLE_REDIRECT_URI
-
-// TODO: Add correct bitbucket redirect uri
-export const BITBUCKET_OAUTH_URL = ''
-// TODO: Add correct facebook redirect uri
-export const FACEBOOK_OAUTH_URL = ''
-
-export const GITHUB_OAUTH_URL = OAUTH_GITHUB_CLIENT_ID && OAUTH_GITHUB_REDIRECT_URI &&
-  `https://github.com/login/oauth/authorize?client_id=${OAUTH_GITHUB_CLIENT_ID}&scope=user:email&redirect_uri=${OAUTH_GITHUB_REDIRECT_URI}`
-
-export const GITLAB_OAUTH_URL = OAUTH_GITLAB_CLIENT_ID && OAUTH_GITLAB_REDIRECT_URI &&
-  `https://gitlab.com/oauth/authorize?client_id=${OAUTH_GITLAB_CLIENT_ID}&response_type=code&scope=read_user&redirect_uri=${OAUTH_GITLAB_REDIRECT_URI}`
-
-// TODO: Add correct facebook redirect uri
-export const GOOGLE_OAUTH_URL = ''
+const DEBUG = process.env.NODE_ENV === 'development'
 
 export const SENTRY_DSN = process.env.FRONTEND_SENTRY_DSN
+
+const DOMAIN = DEBUG ? 'http://localhost:3000' : 'https://recipeyak.com'
+
+const OAUTH_BITBUCKET_CLIENT_ID = process.env.OAUTH_BITBUCKET_CLIENT_ID
+const OAUTH_FACEBOOK_CLIENT_ID = process.env.OAUTH_FACEBOOK_CLIENT_ID
+const OAUTH_GITHUB_CLIENT_ID = process.env.OAUTH_GITHUB_CLIENT_ID
+const OAUTH_GITLAB_CLIENT_ID = process.env.OAUTH_GITLAB_CLIENT_ID
+const OAUTH_GOOGLE_CLIENT_ID = process.env.OAUTH_GOOGLE_CLIENT_ID
+
+const OAUTH_BITBUCKET_REDIRECT_URI = `${DOMAIN}/accounts/bitbucket`
+const OAUTH_FACEBOOK_REDIRECT_URI = `${DOMAIN}/accounts/facebook`
+const OAUTH_GITHUB_REDIRECT_URI = `${DOMAIN}/accounts/github`
+const OAUTH_GITLAB_REDIRECT_URI = `${DOMAIN}/accounts/gitlab`
+const OAUTH_GOOGLE_REDIRECT_URI = `${DOMAIN}/accounts/google`
+
+// TODO: Add Bitbucket, Facebook, and Google
+export const BITBUCKET_OAUTH_URL = ''
+export const FACEBOOK_OAUTH_URL = ''
+export const GOOGLE_OAUTH_URL = ''
+
+export const GITHUB_OAUTH_URL = OAUTH_GITHUB_CLIENT_ID &&
+  `https://github.com/login/oauth/authorize?client_id=${OAUTH_GITHUB_CLIENT_ID}&scope=user:email&redirect_uri=${OAUTH_GITHUB_REDIRECT_URI}`
+
+export const GITLAB_OAUTH_URL = OAUTH_GITLAB_CLIENT_ID &&
+  `https://gitlab.com/oauth/authorize?client_id=${OAUTH_GITLAB_CLIENT_ID}&response_type=code&scope=read_user&redirect_uri=${OAUTH_GITLAB_REDIRECT_URI}`
+
+
