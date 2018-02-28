cc989092b02675ce8f6ed71261f637ed48d110d6
Steve Dignam <steve@dignam.xyz>
2018-02-28 17:43:15 -0500

    Update: react-hot-loader to v4
    This fixes the issue with async lambda functions.
    
    Needed to add babel-env so we don't compile modules (webpack handles it)

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -34,7 +34,6 @@ module.exports = {
   // This means they will be the "root" imports that are included in JS bundle.
   // The first two entry points enable "hot" CSS and auto-refreshes for JS.
   entry: [
-    'react-hot-loader/patch',
     // Include an alternative client for WebpackDevServer. A client's job is to
     // connect to WebpackDevServer by a socket and get notified about changes.
     // When you save a file, the client will either apply hot updates (in case
