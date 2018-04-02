192f9779324f7c762a8d8df47935b236c0686b3b
Steve Dignam <steve@dignam.xyz>
2018-04-02 00:02:28 -0400

    Add types to Settings.tsx

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
 import { Link } from 'react-router-dom'
+import { AxiosError, AxiosResponse } from 'axios'
 
 import Loader from './Loader'
 import { ButtonPrimary, ButtonPlain } from './Buttons'
@@ -11,172 +12,23 @@ import {
 } from './SocialButtons'
 
 import {
-  Provider
-} from '../store/reducers/socialAccounts.ts'
-
-import {
   GITHUB_OAUTH_URL,
   GITLAB_OAUTH_URL,
 } from '../settings'
 
-const Settings = ({
-  avatarURL,
-  email,
-  handleInputChange,
-  updateEmail,
-  updatingEmail,
-  hasPassword,
-  editing,
-  socialAccountConnections,
-  edit,
-  cancelEdit,
-  disconnectAccount,
-  loadingGithub,
-  loadingGitlab,
-  errorGithub,
-  errorGitlab,
-  propsEmail
-}) => {
-  const unchanged = propsEmail === email
-
-  const Github = () =>
-  <div className="mb-2">
-    <div className="d-flex justify-space-between">
-      <div className="d-flex align-items-center">
-        <GithubImg/>
-        Github
-      </div>
-      <div className="d-flex align-center">
-        { socialAccountConnections != null && socialAccountConnections.github != null
-          ? <div className="d-flex align-center flex-wrap">
-              <span className="has-text-success bold">Connected</span>
-              <button onClick={ () => disconnectAccount('github', socialAccountConnections.github) } className={ loadingGithub ? 'is-loading my-button is-danger ml-2' : 'my-button is-danger ml-2' }>Disconnect</button>
-            </div>
-          : <a href={ GITHUB_OAUTH_URL + '/connect' } style={{ 'width': '120px' }} className="my-button ml-2">Connect</a>
-        }
-      </div>
-    </div>
-    { errorGithub && <p className="help is-danger"><b>Error: </b>{ errorGithub }</p> }
-  </div>
-
-  const Gitlab = () =>
-  <div className="mb-2">
-    <div className="d-flex justify-space-between">
-      <div className="d-flex align-items-center">
-        <GitlabImg/>
-        Gitlab
-      </div>
-      <div className="d-flex align-center">
-        { socialAccountConnections != null && socialAccountConnections.gitlab != null
-          ? <div className="d-flex align-center flex-wrap">
-              <span className="has-text-success bold">Connected</span>
-              <button onClick={ () => disconnectAccount('gitlab', socialAccountConnections.gitlab) } className={ loadingGitlab ? 'is-loading my-button is-danger ml-2' : 'my-button is-danger ml-2' }>Disconnect</button>
-            </div>
-          : <a href={ GITLAB_OAUTH_URL + '/connect' } style={{ 'width': '120px' }} className="my-button ml-2">Connect</a>
-        }
-      </div>
-    </div>
-    { errorGitlab && <p className="help is-danger"><b>Error: </b>{ errorGitlab }</p> }
-  </div>
-
-  return <section className="d-grid justify-content-center">
-    <Helmet title='Settings'/>
-
-    <h1 className="fs-8">User settings</h1>
-
-    <div className="d-flex">
-    <a href="https://secure.gravatar.com" className="justify-self-center mr-3">
-      <img className="br-100p" alt="user profile" src={ avatarURL + '&s=128'}/>
-    </a>
-
-    <div className="align-self-center d-flex flex-direction-column">
-    <form
-      className="d-flex align-center"
-      onSubmit={
-        e => {
-          e.preventDefault()
-          if (unchanged) return
-          updateEmail()
-        }
-      }>
-      <label className="better-label">Email</label>
-      { editing
-          ? <input
-              onKeyDown={
-                e => {
-                  if (e.key === 'Escape') {
-                    cancelEdit()
-                  }
-                }
-              }
-              autoFocus
-              defaultValue={ propsEmail }
-              onChange={ handleInputChange }
-              type='text'
-              className='my-input'
-              name='email' />
-          : <span>{ propsEmail }</span>
-      }
-      { editing
-          ? <div className="d-flex">
-            <ButtonPrimary
-              className='ml-2'
-              name="email"
-              type="submit"
-              loading={ updatingEmail }
-              disabled={ unchanged }
-              value='save email'>
-              Save
-            </ButtonPrimary>
-            <ButtonPlain
-              className='ml-2'
-              loading={ updatingEmail }
-              name='email'
-              onClick={ cancelEdit }
-              value='save email'>
-              Cancel
-            </ButtonPlain>
-          </div>
-          : <a
-            className={ 'ml-2 has-text-primary' + (updatingEmail ? ' is-loading' : '') }
-            name='email'
-            onClick={ edit }
-            value='edit'>
-            Edit
-          </a>
-      }
-    </form>
-
-    <div className="d-flex justify-space-between">
-      <label className="better-label">Password</label>
-      { hasPassword
-        ? <Link to="/password" className="has-text-primary">Change Password</Link>
-        : <Link to="/password/set" className="has-text-primary">Set Password</Link>
-      }
-
-    </div>
-  </div>
-</div>
-
-  <h1 className="fs-6">Social Accounts</h1>
-
-  <Github/>
-  <Gitlab/>
-
-  </section>
-}
 
 interface ISettingsProps {
-  disconnectAccount(provider: string, id: number): void
-  updateEmail: boolean
+  disconnectAccount(provider: string, id: number): Promise<AxiosResponse>
+  updateEmail(email: string): Promise<AxiosResponse>
   avatarURL: boolean
   updatingEmail: boolean
   socialAccountConnections: {
-    [key:number]: Provider
+    [key:string]: number
   }
   loading: boolean
   email: string
   fetchData(): void
+  hasPassword: boolean
 }
 
 interface ISettingsState {
@@ -195,12 +47,12 @@ class SettingsWithState extends React.Component<ISettingsProps, ISettingsState> 
     loadingGithub: false,
     errorGithub: '',
     loadingGitlab: false,
-    errorGitlab: false,
+    errorGitlab: '',
     errorGeneral: '',
     editing: false
   }
 
-  componentWillReceiveProps = (nextProps: ISettingsProps) => {
+  componentWillReceiveProps (nextProps: ISettingsProps) {
     this.setState({ email: nextProps.email })
   }
 
@@ -212,7 +64,7 @@ class SettingsWithState extends React.Component<ISettingsProps, ISettingsState> 
     this.setState({ [e.target.name]: e.target.value })
   }
 
-  disconnectAccount = (provider, id) => {
+  disconnectAccount = (provider: string, id: number) => {
     if (provider === 'github') {
       this.setState({ loadingGithub: true, errorGithub: '' })
     } else if (provider === 'gitlab') {
@@ -223,7 +75,7 @@ class SettingsWithState extends React.Component<ISettingsProps, ISettingsState> 
     .then(() => {
       this.setState({ loadingGithub: false, loadingGitlab: false })
     })
-    .catch(error => {
+    .catch((error: AxiosError) => {
       if (error.response && error.response.status === 403 && error.response.data && error.response.data.detail) {
         if (provider === 'github') {
           this.setState({ errorGithub: error.response.data.detail })
@@ -238,50 +90,154 @@ class SettingsWithState extends React.Component<ISettingsProps, ISettingsState> 
   }
 
   render () {
-    const { email, editing, loadingGithub, loadingGitlab, errorGithub, errorGitlab } = this.state
-    const { handleInputChange, disconnectAccount } = this
-
-    const {
-      updateEmail,
-      avatarURL,
-      updatingEmail,
-      socialAccountConnections,
-      loading
-    } = this.props
-
-    if (loading) {
+    if (this.props.loading) {
       return <section className="d-flex justify-content-center">
         <Loader/>
       </section>
     }
+    const edit = () => this.setState({
+      editing: true
+    })
 
-    return (
-      <Settings
-        propsEmail={ this.props.email }
-        email={ email }
-        editing={ editing }
-        edit={ () => this.setState({ editing: true }) }
-        cancelEdit={ () => this.setState({ editing: false }) }
-        handleInputChange={ handleInputChange }
+    const cancelEdit = () => this.setState({
+      editing: false
+    })
 
-        hasPassword={ this.props.hasPassword }
-        socialAccountConnections={ socialAccountConnections }
-        disconnectAccount={ disconnectAccount }
-        loadingGithub={ loadingGithub }
-        loadingGitlab={ loadingGitlab }
-        errorGithub={ errorGithub }
-        errorGitlab={ errorGitlab }
+    const updateEmail = async() => {
+      await this.props.updateEmail(this.state.email)
+      this.setState({
+        editing: false
+      })
+    }
 
-        updateEmail={
-          async () => {
-            await updateEmail(email)
-            this.setState({ editing: false })
-          }
-        }
-        avatarURL={ avatarURL }
-        updatingEmail={ updatingEmail }
-      />
-    )
+    const updatingEmail = this.props.updatingEmail
+
+    const unchanged = this.props.email === this.state.email
+    const Github = () =>
+      <div className="mb-2">
+        <div className="d-flex justify-space-between">
+          <div className="d-flex align-items-center">
+            <GithubImg/>
+            Github
+          </div>
+          <div className="d-flex align-center">
+            { this.props.socialAccountConnections != null && this.props.socialAccountConnections.github != null
+              ? <div className="d-flex align-center flex-wrap">
+                  <span className="has-text-success bold">Connected</span>
+                  <button onClick={ () => this.disconnectAccount('github', this.props.socialAccountConnections.github) } className={ this.state.loadingGithub ? 'is-loading my-button is-danger ml-2' : 'my-button is-danger ml-2' }>Disconnect</button>
+                </div>
+              : <a href={ GITHUB_OAUTH_URL + '/connect' } style={{ 'width': '120px' }} className="my-button ml-2">Connect</a>
+            }
+          </div>
+        </div>
+        { this.state.errorGithub && <p className="help is-danger"><b>Error: </b>{ this.state.errorGithub }</p> }
+      </div>
+
+    const Gitlab = () =>
+      <div className="mb-2">
+        <div className="d-flex justify-space-between">
+          <div className="d-flex align-items-center">
+            <GitlabImg/>
+            Gitlab
+          </div>
+          <div className="d-flex align-center">
+            { this.props.socialAccountConnections != null && this.props.socialAccountConnections.gitlab != null
+              ? <div className="d-flex align-center flex-wrap">
+                  <span className="has-text-success bold">Connected</span>
+                  <button onClick={ () => this.disconnectAccount('gitlab', this.props.socialAccountConnections.gitlab) } className={ this.state.loadingGitlab ? 'is-loading my-button is-danger ml-2' : 'my-button is-danger ml-2' }>Disconnect</button>
+                </div>
+              : <a href={ GITLAB_OAUTH_URL + '/connect' } style={{ 'width': '120px' }} className="my-button ml-2">Connect</a>
+            }
+          </div>
+        </div>
+        { this.state.errorGitlab && <p className="help is-danger"><b>Error: </b>{ this.state.errorGitlab }</p> }
+      </div>
+
+      return (
+        <section className="d-grid justify-content-center">
+          <Helmet title='Settings'/>
+
+          <h1 className="fs-8">User settings</h1>
+
+          <div className="d-flex">
+          <a href="https://secure.gravatar.com" className="justify-self-center mr-3">
+            <img className="br-100p" alt="user profile" src={ this.props.avatarURL + '&s=128'}/>
+          </a>
+
+          <div className="align-self-center d-flex flex-direction-column">
+            <form
+              className="d-flex align-center"
+              onSubmit={
+                e => {
+                  e.preventDefault()
+                  if (unchanged) return
+                  updateEmail()
+                }
+              }>
+              <label className="better-label">Email</label>
+              { this.state.editing
+                  ? <input
+                      onKeyDown={
+                        e => {
+                          if (e.key === 'Escape') {
+                            cancelEdit()
+                          }
+                        }
+                      }
+                      autoFocus
+                      defaultValue={ this.props.email }
+                      onChange={ this.handleInputChange }
+                      type='text'
+                      className='my-input'
+                      name='email' />
+                  : <span>{ this.props.email }</span>
+              }
+              { this.state.editing
+                  ? <div className="d-flex">
+                    <ButtonPrimary
+                      className='ml-2'
+                      name="email"
+                      type="submit"
+                      loading={ updatingEmail }
+                      disabled={ unchanged }
+                      value='save email'>
+                      Save
+                    </ButtonPrimary>
+                    <ButtonPlain
+                      className='ml-2'
+                      loading={ updatingEmail }
+                      name='email'
+                      onClick={ cancelEdit }
+                      value='save email'>
+                      Cancel
+                    </ButtonPlain>
+                  </div>
+                  : <a className={ 'ml-2 has-text-primary' + (updatingEmail ? ' is-loading' : '') }
+                       onClick={ edit }>
+                    Edit
+                  </a>
+              }
+            </form>
+
+            <div className="d-flex justify-space-between">
+              <label className="better-label">Password</label>
+              { this.props.hasPassword
+                ? <Link to="/password" className="has-text-primary">Change Password</Link>
+                : <Link to="/password/set" className="has-text-primary">Set Password</Link>
+              }
+
+            </div>
+          </div>
+        </div>
+
+        <h1 className="fs-6">Social Accounts</h1>
+
+        <Github/>
+        <Gitlab/>
+
+      </section>
+      )
+    }
   }
 }
 
