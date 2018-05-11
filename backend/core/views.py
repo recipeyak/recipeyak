fd68aa22c821ea8fbb554115e07807905899fda2
Christopher Dignam <chris@dignam.xyz>
2018-05-11 00:40:10 -0400

    syntax cleanup

--- backend/core/views.py
+++ backend/core/views.py
@@ -57,7 +57,7 @@ def user_active_team_ids(user):
 
 def add_positions(recipe_steps):
     """Add `position` to step data if missing."""
-    missing_position = any([s.get('position') is None for s in recipe_steps])
+    missing_position = any(s.get('position') is None for s in recipe_steps)
     if missing_position:
         for i, step in enumerate(recipe_steps):
             step['position'] = i + 10.0
