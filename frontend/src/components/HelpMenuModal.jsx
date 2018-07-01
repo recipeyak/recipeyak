b969282fc76f48b46842f8fa805efb5afff7ec1b
Steve Dignam <steve@dignam.xyz>
2018-07-01 13:38:13 -0400

    Update help menu with incr and decr

--- frontend/src/components/HelpMenuModal.jsx
+++ frontend/src/components/HelpMenuModal.jsx
@@ -43,37 +43,43 @@ export default class HelpMenuModal extends React.Component {
         key: '?',
         description: 'toggle help menu',
       },
+      {
+        key: 'A',
+        description: 'increment scheduled recipe amount'
+      },
+      {
+        key: 'X',
+        description: 'decrement scheduled recipe amount'
+      }
     ]
     return (
-      <div>
-        <div className={classNames('modal', { 'is-active': this.state.show })}>
-          <div className="modal-background" onClick={this.close}></div>
-          <div className="modal-content">
-            <div className="box">
-              <section className="d-flex space-between">
-                <h1 className="fs-4 bold">Keybinds</h1>
-                <button className="delete" aria-label="close" onClick={this.close}></button>
-              </section>
-              <section className="d-flex">
-                <div className="mr-4">
-                  { keybinds.map(({ description }) =>
-                    <div className="mb-1" key={description}>
-                      { description }
-                    </div>)
-                  }
-                </div>
-                <div>
-                  { keybinds.map(({ key }) =>
-                    <div className="mb-1" key={key}>
-                      <kbd>{ key }</kbd>
-                    </div>)
-                  }
-                </div>
-              </section>
-            </div>
+      <div className={classNames('modal', { 'is-active': this.state.show })}>
+        <div className="modal-background" onClick={this.close}></div>
+        <div className="modal-content">
+          <div className="box">
+            <section className="d-flex space-between">
+              <h1 className="fs-4 bold">Keybinds</h1>
+              <button className="delete" aria-label="close" onClick={this.close}></button>
+            </section>
+            <section className="d-flex">
+              <div className="mr-4">
+                { keybinds.map(({ description }) =>
+                  <div className="mb-1" key={description}>
+                    { description }
+                  </div>)
+                }
+              </div>
+              <div>
+                { keybinds.map(({ key }) =>
+                  <div className="mb-1" key={key}>
+                    <kbd>{ key }</kbd>
+                  </div>)
+                }
+              </div>
+            </section>
           </div>
-          <button className="modal-close is-large" aria-label="close" onClick={this.close}></button>
         </div>
+        <button className="modal-close is-large" aria-label="close" onClick={this.close}></button>
       </div>
     )
   }
