39088f405b91ec87075771ffb7fc9dce5d14e50c
Steve Dignam <sbdchd@users.noreply.github.com>
2018-07-01 22:46:19 -0400

    Add comma between description and ingredient name (#164)

--- frontend/src/components/IngredientView.jsx
+++ frontend/src/components/IngredientView.jsx
@@ -9,9 +9,13 @@ export default class IngredientView extends React.Component {
       optional,
     } = this.props
 
+    const fmtDescription = description
+      ? ', ' + description
+      : ''
+
     return (
       <p className="listitem-text justify-space-between">
-        { quantity } { name } { description }{ ' ' }
+        { quantity }{ ' ' }{ name }{ fmtDescription }{ ' ' }
         {
           optional
             ? <span className="text-muted">[optional]</span>
