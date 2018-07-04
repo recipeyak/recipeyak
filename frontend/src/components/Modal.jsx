c4879bde0f77a1d655b27fe4c292abf3e36be715
Christopher Dignam <chris@dignam.xyz>
2018-07-04 16:29:14 -0400

    Create modal base component

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { classNames } from '../classnames'
+
+export default class Modal extends React.Component {
+
+  static propTypes = {
+    onClose: PropTypes.func,
+    show: PropTypes.bool.isRequired,
+    children: PropTypes.element.isRequired,
+  }
+
+  static defaultProps = {
+    onClose: () => {},
+  }
+
+  componentWillMount () {
+    document.addEventListener('keydown', this.handleKeyDown)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('keydown', this.handleKeyDown)
+  }
+
+  handleKeyDown = e => {
+    if (e.key === 'Escape') {
+      this.props.onClose()
+    }
+  }
+
+  render () {
+    const { show, children } = this.props
+    return (
+      <div className={classNames('modal', { 'is-active': show })}>
+        <div className="modal-background" onClick={this.close}></div>
+        <div className="modal-content">
+          { children }
+        </div>
+        <button className="modal-close is-large" aria-label="close" onClick={this.close}></button>
+      </div>
+    )
+  }
+}
