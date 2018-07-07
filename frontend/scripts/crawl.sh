8cb0184c5f6dc902af317aec92bd6f05eb3e9e20
Steve Dignam <steve@dignam.xyz>
2018-07-07 10:38:34 -0400

    WIP: Fix build script
    TODO:
    - install puppeteer on CircleCI (we should just use our own image)
    - evaluate not running puppeteer as root

--- frontend/scripts/crawl.sh
+++ frontend/scripts/crawl.sh
@@ -5,50 +5,6 @@ if ! hash serve 2>/dev/null; then
   yarn global add serve
 fi
 
-# install chrome dependencies
-# Mac OS doesn't have this issue, but the CI container does.
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  sudo apt-get install -yq \
-    gconf-service \
-    libasound2 \
-    libatk1.0-0 \
-    libc6 \
-    libcairo2 \
-    libcups2 \
-    libdbus-1-3 \
-    libexpat1 \
-    libfontconfig1 \
-    libgcc1 \
-    libgconf-2-4 \
-    libgdk-pixbuf2.0-0 \
-    libglib2.0-0 \
-    libgtk-3-0 \
-    libnspr4 \
-    libpango-1.0-0 \
-    libpangocairo-1.0-0 \
-    libstdc++6 \
-    libx11-6 \
-    libx11-xcb1 \
-    libxcb1 \
-    libxcomposite1 \
-    libxcursor1 \
-    libxdamage1 \
-    libxext6 \
-    libxfixes3 \
-    libxi6 \
-    libxrandr2 \
-    libxrender1 \
-    libxss1 \
-    libxtst6 \
-    ca-certificates \
-    fonts-liberation \
-    libappindicator1 \
-    libnss3 \
-    lsb-release \
-    xdg-utils \
-    wget
-fi
-
 PORT=8008
 
 serve build -l "$PORT" &
