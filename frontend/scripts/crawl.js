6c56bff13ff60fabaa4834c73c519b331d7dede9
Steve Dignam <sbdchd@users.noreply.github.com>
2018-07-06 21:00:02 -0400

    Add script to do basic crawl to populate index.html (#168)
    Now it is part of `yarn build`

---
+++ frontend/scripts/crawl.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const puppeteer = require('puppeteer')
+
+const HELP = `usage: ./crawl.js url [file]`
+
+function writeFile (filename, content) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, content, (err) => {
+      if (err) {
+        reject(err)
+      }
+      resolve()
+    })
+  })
+}
+
+async function main() {
+  // we slice off the `node` binary and the script filename
+  const args = process.argv.slice(2)
+
+  if (args.length < 1) {
+    console.error(HELP)
+    process.exit(1)
+  }
+  const URL = args[0]
+  const filename = args.length === 2 ? args[1] : null
+
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(URL)
+  const content = await page.content()
+
+  if (filename) {
+    try {
+      await writeFile(filename, content)
+    } catch(e) {
+      console.error(e)
+      await browser.close()
+      process.exit(1)
+    }
+  } else {
+    console.log(content)
+  }
+
+  await browser.close()
+  process.exit(0)
+}
+
+main()
