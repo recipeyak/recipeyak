b4db92491b1af9e1f620fc7b1b8d538da89036ef
Steve Dignam <steve@dignam.xyz>
2018-12-21 12:55:17 -0500

    remove formatting lints from eslint & tslint
    prettier handles this now

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -9,43 +9,17 @@ module.exports = {
   },
   plugins: ["html", "react"],
   // https://github.com/standard/eslint-config-standard/blob/c4902d20cab15971932f591d0d8cf7915ade307f/eslintrc.json
-  extends: ["react-app", "standard"],
+  extends: ["react-app", "standard", "prettier"],
   env: {
     browser: true
   },
   // add your custom rules here
   rules: {
-    // allow paren-less arrow functions
-    "arrow-parens": 0,
     // allow async-await
     "generator-star-spacing": 0,
-    "no-tabs": "warn",
-    "block-spacing": ["warn", "always"],
-    "comma-spacing": ["warn", { before: false, after: true }],
-    "brace-style": ["warn", "1tbs", { allowSingleLine: true }],
-    "comma-dangle": ["warn", "only-multiline"],
-    semi: ["warn", "never"],
-    "padded-blocks": ["warn", "never"],
     "import/first": ["warn", "always"],
-    indent: ["warn", 2],
-    quotes: ["warn", "single", { avoidEscape: true }],
     "one-var": ["warn", "never"],
-    "operator-linebreak": [
-      "warn",
-      "after",
-      { overrides: { "?": "before", ":": "before" } }
-    ],
-    "space-infix-ops": ["warn", { int32Hint: false }],
-    "standard/object-curly-even-spacing": ["warn", "either"],
     "no-constant-condition": ["warn", { checkLoops: false }],
-    "space-before-function-paren": [
-      "warn",
-      {
-        anonymous: "always",
-        named: "always",
-        asyncArrow: "always"
-      }
-    ],
     "no-unused-vars": [
       "warn",
       {
@@ -55,16 +29,11 @@ module.exports = {
         argsIgnorePattern: "^_"
       }
     ],
-    "template-curly-spacing": ["warn", "never"],
     // we don't trigger on properties since the python backend returns json
     // with snake case
     camelcase: ["warn", { properties: "never" }],
-    "key-spacing": ["warn", { afterColon: true }],
     // allow debugger during development
     "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
-    "object-curly-spacing": ["warn", "always"],
-    "no-multiple-empty-lines": ["warn", { max: 1, maxEOF: 0 }],
-    "no-multi-spaces": ["warn"],
     "prefer-const": [
       "error",
       {
@@ -72,22 +41,7 @@ module.exports = {
         ignoreReadBeforeAssign: false
       }
     ],
-    "arrow-spacing": ["warn", { before: true, after: true }],
-    "space-in-parens": ["warn", "never"],
     "no-useless-return": "warn",
-    "no-unreachable": "warn",
-    "space-before-blocks": ["warn", "always"],
-    "spaced-comment": [
-      "warn",
-      "always",
-      {
-        line: { markers: ["*package", "!", "/", ",", "="] },
-        block: {
-          balanced: true,
-          markers: ["*package", "!", ",", ":", "::", "flow-include"],
-          exceptions: ["*"]
-        }
-      }
-    ]
+    "no-unreachable": "warn"
   }
 }
