928de6ffdfd7977fa01f5aeceea99378c2206452
Christopher Dignam <chris@dignam.xyz>
2018-12-23 14:14:28 -0500

    Convert Nav to Typescript

--- frontend/src/components/Nav.test.tsx
+++ frontend/src/components/Nav.test.tsx
@@ -6,16 +6,28 @@ import { mount, configure } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
 
 import { emptyStore as store } from "../store/store"
-import Nav from "./Nav.jsx"
+import Nav from "./Nav"
 
 configure({ adapter: new Adapter() })
 
 describe("<Nav/>", () => {
   it("renders without failure", () => {
+    const props: GetComponentProps<Nav> = {
+      fetchData: jest.fn(),
+      logout: jest.fn(),
+      toggleDarkMode: jest.fn(),
+      avatarURL: "example.com/avatar.png",
+      email: "j.doe@example.com",
+      loggingOut: false,
+      darkMode: false,
+      scheduleURL: "/schedule",
+      teams: [],
+      loadingTeams: false
+    }
     mount(
       <Provider store={store}>
         <MemoryRouter>
-          <Nav fetchData={() => true} />
+          <Nav {...props} />
         </MemoryRouter>
       </Provider>
     )
