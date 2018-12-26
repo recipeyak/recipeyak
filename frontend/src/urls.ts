79a5b0b323cfe612ecb3a4f608cb66ea30ad3930
Christopher Dignam <chris@dignam.xyz>
2018-12-25 23:39:20 -0500

    More generic

--- frontend/src/urls.ts
+++ frontend/src/urls.ts
@@ -4,11 +4,11 @@ export const toURL = (x = "") => encodeURIComponent(x.replace(/\s/g, "-"))
 
 interface IBlah {
   // tslint:disable-next-line: no-any
-  toString(x: any): string
+  toString(): string
 }
 
-export const recipeURL = <T extends IBlah>(id: T, name: string) =>
-  `/recipes/${id}-${toURL(name)}`
+export const recipeURL = <T extends IBlah, U extends IBlah>(id: T, name: U) =>
+  `/recipes/${id}-${toURL(name.toString())}`
 export const teamURL = (id: number, name: string) => `/t/${id}-${toURL(name)}`
 export const inviteURL = (teamID: number, name: string) =>
   teamURL(teamID, name) + "/invite"
