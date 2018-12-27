0a866e903342a462d5cb43a6167b317c7cdfe007
Christopher Dignam <chris@dignam.xyz>
2018-12-27 13:33:57 -0500

    Fix exception from incorrect string conversion of uuid
    We introduced some bugs in d8dc53d474271288bec0b99ceb605832b6273f45. This
    fixes an exception that is being raised when scheduling new recipes.

--- frontend/src/uuid.ts
+++ frontend/src/uuid.ts
@@ -7,3 +7,23 @@ export const uuid4 = () =>
     const v = c === "x" ? r : (r & 0x3) | 0x8
     return v.toString(16)
   })
+
+/**
+ * Generate a random 32bit number
+ *
+ * **Note:** We are limited to 32bit ints because of JS. An int UUID must be
+ * 128 bits, so this is not considered unique, however, for our frontend
+ * purposes, it is acceptable
+ *
+ * @example
+ *
+ * const id = random32Id()
+ *
+ * const pendingRecipe = {
+ *  ...existingRecipe,
+ *  id,
+ *  pending: true
+ * }
+ *
+ */
+export const random32Id = () => crypto.getRandomValues(new Uint32Array(1))[0]
