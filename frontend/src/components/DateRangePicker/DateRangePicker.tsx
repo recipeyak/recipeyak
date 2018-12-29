b579e58bdc8e14da71310a751eee1031f3985ee1
Christopher Dignam <chris@dignam.xyz>
2018-12-29 01:53:15 -0500

    Add forgotten files

--- frontend/src/components/DateRangePicker/DateRangePicker.tsx
+++ frontend/src/components/DateRangePicker/DateRangePicker.tsx
@@ -5,6 +5,7 @@ import endOfDay from "date-fns/end_of_day"
 
 import { classNames } from "@/classnames"
 import Month from "@/components/DateRangePicker/Month"
+import GlobalEvent from "@/components/GlobalEvent"
 
 import "@/components/DateRangePicker/date-range-picker.scss"
 
@@ -19,9 +20,12 @@ interface IDateRangePickerProps {
   readonly endDay: Date
   readonly nextMonth: () => void
   readonly prevMonth: () => void
+  readonly onClose: () => void
 }
 
 class DateRangePicker extends React.Component<IDateRangePickerProps> {
+  element = React.createRef<HTMLDivElement>()
+
   handleClick = (date: Date) => {
     if (isPast(endOfDay(date))) {
       return
@@ -36,10 +40,18 @@ class DateRangePicker extends React.Component<IDateRangePickerProps> {
     }
   }
 
+  handleGeneralClick = (e: MouseEvent) => {
+    const el = this.element.current
+    if (el && e.target && !el.contains(e.target as Node)) {
+      // outside click
+      this.props.onClose()
+    }
+  }
+
   render() {
     return (
       <div
-        id="date-range-picker"
+        ref={this.element}
         className={classNames(
           "p-absolute",
           "box-shadow-normal",
@@ -50,6 +62,7 @@ class DateRangePicker extends React.Component<IDateRangePickerProps> {
           "grid-2-months",
           this.props.visible ? "d-grid" : "d-none"
         )}>
+        <GlobalEvent mouseDown={this.handleGeneralClick} />
         <Month
           showLeft
           date={this.props.month}
