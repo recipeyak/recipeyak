b0933e832481e7313771f5f2ec32dddf241ab505
Steve Dignam <steve@dignam.xyz>
2018-12-30 18:30:01 -0500

    refactor schedule page sidebar into its own component

--- frontend/src/components/Schedule.tsx
+++ frontend/src/components/Schedule.tsx
@@ -10,6 +10,60 @@ import ShoppingList from "@/components/ShoppingList"
 
 import { Dispatch, updatingTeamID } from "@/store/actions"
 import { Tabs, Tab } from "@/components/Tabs"
+import { ITeam } from "@/store/reducers/teams"
+
+interface ISidebarProps {
+  readonly isRecipes: boolean
+  readonly closed: boolean
+  readonly teamID: ITeam["id"] | "personal"
+  readonly toggleClose: () => void
+}
+
+function Sidebar({ closed, teamID, isRecipes, toggleClose }: ISidebarProps) {
+  const arrow = closed ? "→" : "←"
+
+  const sideBarStyle = {
+    display: closed ? "none" : "grid"
+  }
+
+  const recipesURL =
+    teamID === "personal"
+      ? "/schedule/recipes"
+      : `/t/${teamID}/schedule/recipes`
+
+  const shoppingURL =
+    teamID === "personal"
+      ? "/schedule/shopping"
+      : `/t/${teamID}/schedule/shopping`
+
+  return (
+    <>
+      <div
+        className="d-grid grid-gap-4 grid-auto-rows-min-content w-350px-if-not-sm"
+        style={sideBarStyle}>
+        <Tabs small className="mb-0 no-print">
+          <Tab isActive={!isRecipes}>
+            <Link to={shoppingURL}>Shopping</Link>
+          </Tab>
+          <Tab isActive={isRecipes}>
+            <Link to={recipesURL}>Recipes</Link>
+          </Tab>
+        </Tabs>
+
+        {isRecipes ? (
+          <Recipes teamID={teamID} scroll drag noPadding />
+        ) : (
+          <ShoppingList teamID={teamID} />
+        )}
+      </div>
+      <a
+        className="select-none closer text-decoration-none no-print hide-sm"
+        onClick={toggleClose}>
+        {arrow}
+      </a>
+    </>
+  )
+}
 
 export type ScheduleRouteParams = RouteComponentProps<{
   id?: string
@@ -17,8 +71,8 @@ export type ScheduleRouteParams = RouteComponentProps<{
 }>
 
 interface IScheduleProps extends ScheduleRouteParams {
-  readonly updateTeamID: (id: number | null) => void
-  readonly teamID: number | null
+  readonly updateTeamID: (id: ITeam["id"] | null) => void
+  readonly teamID: ITeam["id"] | null
   readonly type: "shopping" | "recipes"
 }
 
@@ -44,54 +98,20 @@ class Schedule extends React.Component<IScheduleProps, IScheduleState> {
   }
 
   render() {
-    const isRecipes = this.props.type === "recipes"
-
-    const arrow = this.state.closed ? "→" : "←"
-
-    const sideBarStyle = {
-      display: this.state.closed ? "none" : "grid"
-    }
-
     const teamID = this.props.teamID || "personal"
 
-    const recipesURL =
-      teamID === "personal"
-        ? "/schedule/recipes"
-        : `/t/${teamID}/schedule/recipes`
-
-    const shoppingURL =
-      teamID === "personal"
-        ? "/schedule/shopping"
-        : `/t/${teamID}/schedule/shopping`
-
+    const isRecipes = this.props.type === "recipes"
     return (
       <div
         className="d-flex pl-2 pr-2"
         style={{ height: "calc(100vh - 80px)" }}>
         <Helmet title="Schedule" />
-        <div
-          className="d-grid grid-gap-4 grid-auto-rows-min-content w-350px-if-not-sm"
-          style={sideBarStyle}>
-          <Tabs small className="mb-0 no-print">
-            <Tab isActive={!isRecipes}>
-              <Link to={shoppingURL}>Shopping</Link>
-            </Tab>
-            <Tab isActive={isRecipes}>
-              <Link to={recipesURL}>Recipes</Link>
-            </Tab>
-          </Tabs>
-
-          {isRecipes ? (
-            <Recipes teamID={teamID} scroll drag noPadding />
-          ) : (
-            <ShoppingList teamID={teamID} />
-          )}
-        </div>
-        <a
-          className="select-none closer text-decoration-none no-print hide-sm"
-          onClick={this.toggleClose}>
-          {arrow}
-        </a>
+        <Sidebar
+          toggleClose={this.toggleClose}
+          teamID={teamID}
+          closed={this.state.closed}
+          isRecipes={isRecipes}
+        />
         <Calendar type={this.props.type} teamID={teamID} />
       </div>
     )
