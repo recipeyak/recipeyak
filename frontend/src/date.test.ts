02e3cf8e85efa926720395b6e5c6fdf8e5693b96
Steve Dignam <steve@dignam.xyz>
2018-12-30 17:28:37 -0500

    have calendar only display three weeks at a time
    refactor calendar into smaller components

--- frontend/src/date.test.ts
+++
@@ -1,8 +0,0 @@
-import { daysUntilSaturday } from "@/date"
-
-describe("date", () => {
-  it("provides number of days from saturday", () => {
-    const date = new Date("2018-5-31")
-    expect(daysUntilSaturday(date)).toEqual(2)
-  })
-})
