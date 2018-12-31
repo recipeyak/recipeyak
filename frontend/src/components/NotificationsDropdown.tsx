69134b9026032415e9483b5062b8e80d93a90b56
Christopher Dignam <chris@dignam.xyz>
2018-12-31 16:26:39 -0500

    Rename NavDropdown for a future PR

--- frontend/src/components/NotificationsDropdown.tsx
+++ frontend/src/components/NotificationsDropdown.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Dropdown from "@/components/Dropdown"
+import Dropdown from "@/components/NavDropdown"
 import { Link } from "react-router-dom"
 import { ButtonPrimary } from "@/components/Buttons"
 
