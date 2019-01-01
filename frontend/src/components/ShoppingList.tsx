91a7d8dbfe75a6330ecdd713ef18e7e8b3330162
Christopher Dignam <chris@dignam.xyz>
2019-01-01 01:05:11 -0500

    Fix date range picker behavior in shopping list
    We can select ranges again.

--- frontend/src/components/ShoppingList.tsx
+++ frontend/src/components/ShoppingList.tsx
@@ -176,9 +176,8 @@ class ShoppingList extends React.Component<
   render() {
     return (
       <div className="d-grid grid-gap-2">
-        <div className="p-rel">
+        <div className="p-rel" ref={this.element}>
           <div
-            ref={this.element}
             className="d-flex align-items-center no-print">
             <GlobalEvent mouseDown={this.handleGeneralClick} />
             <input
