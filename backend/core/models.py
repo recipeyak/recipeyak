3627820152033ba0f87779cc41b6aa3dfc333dbd
Christopher Dignam <chris@dignam.xyz>
2019-01-02 22:24:27 -0500

    Hint

--- backend/core/models.py
+++ backend/core/models.py
@@ -161,7 +161,7 @@ class Recipe(CommonInfo):
             self.save()
             return self
 
-    def copy_to(self, account) -> "Recipe":
+    def copy_to(self, account: Union[MyUser, Team]) -> "Recipe":
         """
         Copy recipe to another team or user
         """
