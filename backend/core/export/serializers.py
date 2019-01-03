15d932a9eb09c4207f0e509dafdb069ec35700bb
Christopher Dignam <chris@dignam.xyz>
2019-01-03 13:11:15 -0500

    Restrict

--- backend/core/export/serializers.py
+++ backend/core/export/serializers.py
@@ -1,23 +1,25 @@
 from rest_framework import serializers
 
 from core.recipes.serializers import IngredientSerializer, OwnerRelatedField
-
 from core.models import Recipe
+from core.serialization import BaseModelSerializer
 
 
-class RecipeExportSerializer(serializers.ModelSerializer):
+class RecipeExportSerializer(BaseModelSerializer):
 
-    steps = serializers.ListField(child=serializers.CharField())
+    steps = serializers.ListField(child=serializers.CharField(), source='step_set.all')
 
     ingredients = IngredientSerializer(
         many=True,
         read_only=True,
         fields=("quantity", "name", "description", "optional"),
+        source='ingredient_set'
     )
     owner = OwnerRelatedField(read_only=True, export=True)
 
     class Meta:
         model = Recipe
+        read_only = True
         fields = (
             "name",
             "author",
