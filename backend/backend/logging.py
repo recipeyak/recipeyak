947b490f1ce18c79612acd5e743ce646f5c5000e
Christopher Dignam <chris@dignam.xyz>
2019-02-09 23:29:32 -0500

    Enable logging in dev mode, but disable in testing

---
+++ backend/backend/logging.py
@@ -0,0 +1,11 @@
+import os
+from django.conf import settings
+
+
+class TestingDisableFilter:
+    """
+    Disable logging if the TESTING environment variable is set.
+    """
+
+    def filter(self, record):
+        return not settings.TESTING
