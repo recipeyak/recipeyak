0f078d5f1459c7d1088759d7590c085a2c234893
Steve Dignam <steve@dignam.xyz>
2019-02-26 23:23:17 -0500

    Update landing page with new images & copy

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -4,21 +4,163 @@ import Footer from "@/components/Footer"
 
 // tslint:disable:no-var-requires
 const addRecipeImg = require("@/static/images/pages/add-recipe.png")
-const cartDoublingImg = require("@/static/images/pages/cart-doubling.png")
-const cartImg = require("@/static/images/pages/cart.png")
-const homepageImg = require("@/static/images/pages/homepage.png")
-const listImg = require("@/static/images/pages/list.png")
+const landingImg = require("@/static/images/pages/schedule.png")
+const teamImg = require("@/static/images/pages/team.png")
 const searchImg = require("@/static/images/pages/search.png")
+const shopImg = require("@/static/images/pages/shop.png")
+const copyShoppingList = require("@/static/images/pages/copy-shopping-list.png")
 // tslint:enable:no-var-requires
 
 const isOdd = (i: number) => i % 2 !== 0
 
+interface IFeaturesContainerProps {
+  readonly children: JSX.Element[]
+}
+
+function FeaturesContainer({ children }: IFeaturesContainerProps) {
+  return (
+    <section className="bg-primary color-white pt-4 pb-4 pr-4 pl-4">
+      <section className="home-container">
+        <h2 className="home-subtitle bold">Features</h2>
+        <ul className="d-grid grid-gap-1rem">{children}</ul>
+      </section>
+    </section>
+  )
+}
+
+interface IFeatureProps {
+  readonly text: string
+  readonly imageURL: string
+  readonly index: number
+}
+
+function Feature({ text, imageURL, index }: IFeatureProps) {
+  return (
+    <li className="feature-grid" key={text}>
+      <p
+        className={`align-self-center ${
+          isOdd(index) ? "grid-column-2 grid-row-1" : ""
+        }`}>
+        {text}
+      </p>
+      <div className="fact-img align-self-center ">
+        <img className="box-shadow-normal " src={imageURL} alt="" />
+      </div>
+    </li>
+  )
+}
+
+interface IHowItWorksContainerProps {
+  readonly children: React.ReactNode
+}
+function HowItWorksContainer({ children }: IHowItWorksContainerProps) {
+  return (
+    <section className="pt-4 pr-4 pl-4">
+      <section className="home-container d-grid">
+        <h2 className="home-subtitle bold">How it works</h2>
+        <ol className="d-grid grid-gap-2rem">{children}</ol>
+      </section>
+    </section>
+  )
+}
+interface IHowToProps {
+  readonly content: React.ReactNode
+  readonly imageURL: string
+  readonly index: number
+}
+
+function HowTo({ content, index, imageURL }: IHowToProps) {
+  // tslint:disable-next-line:no-unsafe-any
+  return (
+    <li className="feature-grid" key={imageURL}>
+      <p className="align-self-center">
+        <b>
+          {index + 1}
+          {". "}
+        </b>
+        {content}
+      </p>
+      <div className="fact-img">
+        {/* tslint:disable-next-line:no-unsafe-any */}
+        <img className="box-shadow-normal " src={imageURL} alt="" />
+      </div>
+    </li>
+  )
+}
+
+const features = [
+  {
+    text:
+      "Full text recipe search. Easily find recipes by ingredient, author, and name.",
+    imgURL: searchImg
+  },
+  {
+    text:
+      "Collaborate using Recipe Yak Teams to create a shared recipe schedule and shopping list.",
+    imgURL: teamImg
+  },
+  {
+    text:
+      "Automatically generate a condensed shopping list when selecting days to shop.",
+    imgURL: copyShoppingList
+  }
+]
+
+const howToSteps = [
+  {
+    text: (
+      <span>
+        After logging in, add your recipes via the{" "}
+        <Link className="text-decoration-underline" to="/recipes/add">
+          add recipe
+        </Link>{" "}
+        form.
+      </span>
+    ),
+    imgURL: addRecipeImg
+  },
+  {
+    text: (
+      <span>
+        Navigate to the{" "}
+        <Link to="/schedule" className="text-decoration-underline">
+          schedule page
+        </Link>{" "}
+        and add drag recipes onto your calendar.
+      </span>
+    ),
+    imgURL: landingImg
+  },
+  {
+    text: (
+      <span>
+        Navigate to the{" "}
+        <Link to="/schedule/shopping" className="text-decoration-underline">
+          shopping list
+        </Link>{" "}
+        and adjust the days you are shopping for.
+      </span>
+    ),
+    imgURL: shopImg
+  },
+  {
+    text: (
+      <span>
+        Copy the automatically generated shopping list with one click.
+      </span>
+    ),
+    imgURL: copyShoppingList
+  }
+]
+
 const LandingPage = () => (
   <>
-    <section className="container d-grid grid-gap-1rem pb-4 pr-4 pl-4">
-      <h1 className="home-hero-text">
-        A place to store, share, and create recipes
-      </h1>
+    <section className="home-container d-grid grid-gap-1rem pb-4 pr-4 pl-4">
+      <section className="d-flex justify-content-center mb-2">
+        <h1 className="home-hero-text">
+          A place to store, share, and create recipes
+        </h1>
+      </section>
 
       <Link
         to="/signup"
@@ -27,119 +169,32 @@ const LandingPage = () => (
       </Link>
     </section>
     <section className="pt-4 bg-50-50-primary pr-4 pl-4">
-      <section className="container">
+      <section className="home-container">
         {/* tslint:disable-next-line:no-unsafe-any */}
-        <img className="box-shadow-normal" src={homepageImg} alt="" />
+        <img className="box-shadow-normal" src={landingImg} alt="" />
       </section>
     </section>
 
-    <section className="bg-primary color-white pt-4 pb-4 pr-4 pl-4">
-      <section className="container">
-        <h2 className="home-subtitle bold">Features</h2>
-        <ul className="d-grid grid-gap-1rem">
-          {[
-            {
-              text:
-                "Sift through through your entire recipe collection in milliseconds with the enhanced search box.",
-              imgURL: searchImg
-            },
-            {
-              text:
-                "Keep all your recipes in one place with unlimited recipe storage",
-              imgURL: listImg
-            },
-            {
-              text:
-                "Automatically generate a condensed shopping list by adding recipes to your cart.",
-              imgURL: cartImg
-            },
-            {
-              text:
-                "Easily double recipes and have the resulting ingredients appear in your shopping list",
-              imgURL: cartDoublingImg
-            }
-          ].map(({ text, imgURL }, i) => (
-            // tslint:disable-next-line:no-unsafe-any
-            <li className="feature-grid" key={text}>
-              <p
-                className={`align-self-center ${
-                  isOdd(i) ? "grid-column-2 grid-row-1" : ""
-                }`}>
-                {text}
-              </p>
-              <div className="fact-img align-self-center ">
-                {/* tslint:disable-next-line:no-unsafe-any */}
-                <img className="box-shadow-normal " src={imgURL} alt="" />
-              </div>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </section>
+    <FeaturesContainer>
+      {features.map(({ text, imgURL }, i) => (
+        // tslint:disable-next-line:no-unsafe-any
+        <Feature text={text} imageURL={imgURL} index={i} />
+      ))}
+    </FeaturesContainer>
 
-    <section className="pt-4 pr-4 pl-4">
-      <section className="container d-grid">
-        <h2 className="home-subtitle bold">How it works</h2>
-        <ol className="d-grid grid-gap-2rem">
-          {[
-            {
-              text: (
-                <span>
-                  After logging in, add your recipes to the store via the{" "}
-                  <Link className="text-decoration-underline" to="/recipes/add">
-                    Add Recipe
-                  </Link>{" "}
-                  form.
-                </span>
-              ),
-              imgURL: addRecipeImg
-            },
-            {
-              text: (
-                <span>
-                  Search through your{" "}
-                  <Link to="/recipes" className="text-decoration-underline">
-                    recipe list
-                  </Link>{" "}
-                  and add recipes to your cart.
-                </span>
-              ),
-              imgURL: searchImg
-            },
-            {
-              text: (
-                <span>
-                  Double any necessary recipes and print the automatically
-                  generated shopping list.
-                </span>
-              ),
-              imgURL: cartDoublingImg
-            }
-          ].map(({ text, imgURL }, i) => (
-            // tslint:disable-next-line:no-unsafe-any
-            <li className="feature-grid" key={imgURL}>
-              <p className="align-self-center">
-                <b>
-                  {i + 1}
-                  {". "}
-                </b>
-                {text}
-              </p>
-              <div className="fact-img">
-                {/* tslint:disable-next-line:no-unsafe-any */}
-                <img className="box-shadow-normal " src={imgURL} alt="" />
-              </div>
-            </li>
-          ))}
-        </ol>
+    <HowItWorksContainer>
+      {howToSteps.map(({ text, imgURL }, i) => (
+        // tslint:disable-next-line:no-unsafe-any
+        <HowTo content={text} imageURL={imgURL} index={i} />
+      ))}
 
-        <Link
-          to="/signup"
-          className="my-button is-primary is-large justify-self-center mt-4 mb-2">
-          Create Account
-        </Link>
-      </section>
-    </section>
+      <Link
+        to="/signup"
+        className="my-button is-primary is-large justify-self-center mt-4 mb-2">
+        Create Account
+      </Link>
+    </HowItWorksContainer>
+
     <Footer />
   </>
 )
