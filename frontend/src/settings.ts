7b2d6aa7a2a01bc66dbab8fc2a7103891aebc289
Christopher Dignam <chris@dignam.xyz>
2019-03-10 13:25:38 -0400

    lint

--- frontend/src/settings.ts
+++ frontend/src/settings.ts
@@ -10,18 +10,18 @@ const DOMAIN = DEBUG ? "http://localhost:3000" : "https://recipeyak.com"
 
 // TODO(sbdchd): we export these as a quick hack since we aren't using them
 export const OAUTH_BITBUCKET_CLIENT_ID = process.env.OAUTH_BITBUCKET_CLIENT_ID
-const OAUTH_FACEBOOK_CLIENT_ID = process.env.OAUTH_FACEBOOK_CLIENT_ID
+export const OAUTH_FACEBOOK_CLIENT_ID = process.env.OAUTH_FACEBOOK_CLIENT_ID
 const OAUTH_GITHUB_CLIENT_ID = process.env.OAUTH_GITHUB_CLIENT_ID
 const OAUTH_GITLAB_CLIENT_ID = process.env.OAUTH_GITLAB_CLIENT_ID
 const OAUTH_GOOGLE_CLIENT_ID = process.env.OAUTH_GOOGLE_CLIENT_ID
 
 export const OAUTH_BITBUCKET_REDIRECT_URI = `${DOMAIN}/accounts/bitbucket`
-const OAUTH_FACEBOOK_REDIRECT_URI = `${DOMAIN}/accounts/facebook`
+export const OAUTH_FACEBOOK_REDIRECT_URI = `${DOMAIN}/accounts/facebook`
 const OAUTH_GITHUB_REDIRECT_URI = `${DOMAIN}/accounts/github`
 const OAUTH_GITLAB_REDIRECT_URI = `${DOMAIN}/accounts/gitlab`
 const OAUTH_GOOGLE_REDIRECT_URI = `${DOMAIN}/accounts/google`
 
-// TODO: Add Bitbucket, Facebook, and Google
+// TODO: Add Bitbucket and Facebook
 export const BITBUCKET_OAUTH_URL = ""
 export const FACEBOOK_OAUTH_URL = ``
 export const GOOGLE_OAUTH_URL = `https://accounts.google.com/o/oauth2/auth?client_id=${OAUTH_GOOGLE_CLIENT_ID}&redirect_uri=${OAUTH_GOOGLE_REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`
