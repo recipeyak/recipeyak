73b4326029d59aa087201ae0cdefb359d82105b8
Christopher Dignam <chris@dignam.xyz>
2019-05-26 17:54:39 -0400

    fmt

--- backend/core/users/tests.py
+++ backend/core/users/tests.py
@@ -22,7 +22,4 @@ def test_avatar_url(user):
     """
     User's avatar url should not change
     """
-    assert (
-        user.avatar_url
-        == "/avatar/bc6a715808d9aae0ddeefb1e47e482a6?d=identicon&r=g"
-    )
+    assert user.avatar_url == "/avatar/bc6a715808d9aae0ddeefb1e47e482a6?d=identicon&r=g"
