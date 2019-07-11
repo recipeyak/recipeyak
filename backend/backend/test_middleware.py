e901e304bfb942d8dca8824bd3daa6fddfc2061f
Steve Dignam <steve@dignam.xyz>
2019-07-10 22:57:48 -0400

    add: django healthcheck endpoints

--- backend/backend/test_middleware.py
+++ backend/backend/test_middleware.py
@@ -1,5 +1,7 @@
 import pytest
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.test import APIClient
 
 from backend.middleware import ServerTimingMiddleware
 
@@ -13,3 +15,13 @@ def test_server_timing_middleware():
     server_timing_middleware = ServerTimingMiddleware(get_response)
 
     assert server_timing_middleware("test")["Server-Timing"] is not None
+
+
+def test_health_check_middleware(client: APIClient) -> None:
+    """
+    smoke test for the health check endpoints
+    """
+    res = client.get("/healthz")
+    assert res.status_code == status.HTTP_200_OK
+    res = client.get("/readiness")
+    assert res.status_code == status.HTTP_200_OK
