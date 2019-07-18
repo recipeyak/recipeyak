fb73cd293ef22c77478c95eff3e9f2c1fc81735f
Steve Dignam <steve@dignam.xyz>
2019-07-17 21:19:35 -0400

    explicitly set bash options

--- postgres/pg_dump.sh
+++ postgres/pg_dump.sh
@@ -1,3 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -o xtrace
+set -o pipefail
+set -o errexit
+set -o nounset
 
 pg_dump "$POSTGRES_DB_NAME" | gzip | aws s3 cp - "s3://$POSTGRES_BACKUP_S3_BUCKET/backup-$(date '+%Y-%m-%d-%H-%M-%S')"
