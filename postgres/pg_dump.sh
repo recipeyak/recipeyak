27d35f766b6558062c6a1a91c5308d7c4ec75321
Steve Dignam <steve@dignam.xyz>
2019-07-20 10:40:24 -0400

    pass env vars to pgdump

--- postgres/pg_dump.sh
+++ postgres/pg_dump.sh
@@ -4,4 +4,9 @@ set -o pipefail
 set -o errexit
 set -o nounset
 
-pg_dump "$POSTGRES_DB_NAME" | gzip | aws s3 cp - "s3://$POSTGRES_BACKUP_S3_BUCKET/backup-$(date '+%Y-%m-%d-%H-%M-%S')"
+pg_dump "$POSTGRES_DB_NAME" \
+  -h "$POSTGRES_HOST_NAME" \
+  -p "$POSTGRES_PORT" \
+  -U "$POSTGRES_USERNAME" \
+  | gzip \
+  | aws s3 cp - "s3://$POSTGRES_BACKUP_S3_BUCKET/$(date -u +"%Y-%m-%dT%H:%M:%SZ")-db.sql.gz"
