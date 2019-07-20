20b6da80e3327b46372e635872927a93870a3fa7
Steve Dignam <steve@dignam.xyz>
2019-07-20 16:53:08 -0400

    change to 10min intervals

--- postgres/cron.sh
+++ postgres/cron.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+set -o xtrace
+set -o pipefail
+set -o errexit
+set -o nounset
 
 # Note: getting cron script to see the env vars is a little tricky so we add
 # them to a file and souce that in the crontab file.
