db176be7807dc317f32d922b4fff7a9d621c0b14
Steve Dignam <steve@dignam.xyz>
2019-10-16 19:30:53 -0400

    remove: swagger schema generation
    Swagger was a nice idea on paper, but in practice it doesn't work well
    with DRF.
    
    Need something with more types like FastAPI.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 from django.conf.urls import include
 from django.urls import path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
@@ -63,17 +61,3 @@ urlpatterns = [
         name="team-recipes",
     ),
 ]
-
-if settings.DEBUG:
-    schema_view = get_schema_view(
-        openapi.Info(title="Recipe Yak API", default_version="v1"),
-        public=True,
-        permission_classes=(permissions.AllowAny,),
-    )
-    urlpatterns.append(
-        path(
-            "swagger/",
-            schema_view.with_ui("swagger", cache_timeout=0),
-            name="schema-swagger-ui",
-        )
-    )
