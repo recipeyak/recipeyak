119c6f1f1984f01eddb781cced6d0ca37e00a542
Steve Dignam <sbdchd@users.noreply.github.com>
2019-10-20 14:53:41 -0400

    add(CI): shellcheck (#526)

--- frontend/scripts/crawl.sh
+++ frontend/scripts/crawl.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 set -eu
 
-cd $(dirname $(dirname "$0"))
+cd "$(dirname "$(dirname "$0")")"
 
 PORT=8808
 
-
-cd build
+pushd build || exit
 
 python3 -m http.server "$PORT" &
 
-cd ..
+popd
+
+HTTP_SERVER=$!
 
 # wait for port
 MAX_INC=20
@@ -18,7 +19,7 @@ INC=0
 INTERVAL=0.1
 while ! nc -z localhost "$PORT"; do
   sleep "$INTERVAL"
-  INC=$(($INC + 1))
+  INC=$((INC + 1))
   if [[ "$INC" -ge "$MAX_INC" ]]; then
     exit 1
   fi
@@ -29,7 +30,7 @@ TMP_FILE=$(mktemp)
 ./scripts/crawl.js http://localhost:"$PORT" "$TMP_FILE"
 
 # kill the background webserver
-kill %%
+kill "$HTTP_SERVER"
 
 mv -f "$TMP_FILE" build/landing.html
 chmod +r build/landing.html
