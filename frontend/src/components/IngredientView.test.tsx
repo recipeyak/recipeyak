bfc33510d066af8639ddabc0372edd76b99b4dc6
Steve Dignam <steve@dignam.xyz>
2019-10-21 22:12:09 -0400

    add: auto casing for teaspoon & tablespoon
    teaspoon and tablespoon are less easy to mix up with they have distinct
    casing that is always enforced.

---
+++ frontend/src/components/IngredientView.test.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import { TestProvider } from "@/testUtils"
+import IngredientView from "@/components/IngredientView"
+
+describe("<IngredientView/>", () => {
+  test("snap with capitalization", () => {
+    const testCases = [
+      "3 tablespoons",
+      "1 teaspoon",
+      "1 tablespoon + 2 teaspoons",
+      "1 Teaspoon"
+    ]
+
+    testCases.forEach(quantity => {
+      const tree = renderer
+        .create(
+          <TestProvider>
+            <IngredientView
+              quantity={quantity}
+              name="salt"
+              optional={false}
+              description=""
+            />
+          </TestProvider>
+        )
+        .toJSON()
+
+      expect(tree).toMatchSnapshot()
+    })
+  })
+})
