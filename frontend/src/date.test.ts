4cdf487c4dfe14eb968b459bc5973af5658ba9cf
Steve Dignam <steve@dignam.xyz>
2019-11-09 22:12:57 -0500

    fix comment

--- frontend/src/date.test.ts
+++ frontend/src/date.test.ts
@@ -5,8 +5,9 @@ test("toISODateString", () => {
   // Regression test for previous toString util function that didn't handle
   // timezones correctly so each call would shift the time back a day
 
-  // We want to be using a local date like the browser, since `new
-  // Date("2011-11-09")` is different in the browser depending on the timezone.
+  // We want to be using a local date like the browser, since
+  // `new Date("2011-11-09")` is different in the browser depending on the
+  // timezone.
   mockTimezone("US/Pacific", () => {
     expect(new Date().getTimezoneOffset()).not.toBe(0)
     expect(toISODateString(new Date())).toEqual(
