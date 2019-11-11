ab8ffb4d9ea36b5d22dc769596e2b3bd5d8681a5
Steve Dignam <steve@dignam.xyz>
2019-11-10 19:25:42 -0500

    fix tests

--- frontend/src/components/Nav.test.tsx
+++ frontend/src/components/Nav.test.tsx
@@ -2,11 +2,29 @@ import React from "react"
 import { Navbar } from "@/components/Nav"
 import { TestProvider } from "@/testUtils"
 import renderer from "react-test-renderer"
+import { createEmptyStore } from "@/store/store"
 
 describe("<Nav/>", () => {
-  it("renders without failure", () => {
+  it("renders default", () => {
     const tree = renderer.create(
       <TestProvider>
+        <Navbar />
+      </TestProvider>
+    )
+    expect(tree).toMatchSnapshot()
+  })
+
+  test("render when logged in", () => {
+    const emptyState = createEmptyStore().getState()
+    const store = createEmptyStore({
+      ...emptyState,
+      user: {
+        ...emptyState.user,
+        loggedIn: true
+      }
+    })
+    const tree = renderer.create(
+      <TestProvider store={store}>
         <Navbar />
       </TestProvider>
     )
