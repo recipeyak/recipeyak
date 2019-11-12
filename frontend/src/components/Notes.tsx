2a97755376f692a29b1157750cbb0397e99f4a94
Christopher Dignam <chris@dignam.xyz>
2019-11-11 23:56:17 -0500

    start note creator

--- frontend/src/components/Notes.tsx
+++ frontend/src/components/Notes.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { distanceInWordsToNow } from "date-fns"
 import { Avatar } from "@/components/Avatar"
 import { IRecipe } from "@/store/reducers/recipes"
+import { ButtonPrimary, ButtonSecondary } from "@/components/Buttons"
 
 interface INoteProps {
   readonly note: IRecipe["notes"][0]
@@ -10,7 +11,7 @@ export function Note({ note }: INoteProps) {
   const created_by = note.created_by
   return (
     <div className="d-flex align-items-start">
-      <Avatar avatarURL={created_by.avatar_url} />
+      <Avatar avatarURL={created_by.avatar_url} className="mr-2" />
       <div>
         <p>
           {created_by.email} |{" "}
@@ -23,6 +24,23 @@ export function Note({ note }: INoteProps) {
   )
 }
 
+function NoteCreator() {
+  return (
+    <div>
+      <textarea
+        className="my-textarea mb-2"
+        cols={30}
+        rows={10}
+        placeholder="Add a note..."
+      />
+      <div className="d-flex justify-between align-center">
+        <ButtonPrimary size="small">add</ButtonPrimary>
+        <ButtonSecondary size="small">cancel</ButtonSecondary>
+      </div>
+    </div>
+  )
+}
+
 interface INoteContainerProps {
   readonly recipeId: IRecipe["id"]
   readonly notes: IRecipe["notes"]
@@ -30,6 +48,8 @@ interface INoteContainerProps {
 export function NoteContainer(props: INoteContainerProps) {
   return (
     <>
+      <NoteCreator />
+      <hr />
       {props.notes.map(note => (
         <Note note={note} />
       ))}
