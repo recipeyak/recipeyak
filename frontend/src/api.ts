e6c176f8c980e8fdbabc10b18adf93d46ade89fe
Steve Dignam <steve@dignam.xyz>
2020-02-17 00:37:11 -0500

    add(schedule): refetch on focus change
    If a tab has been open for a while it's nice if when you come back to it
    the data is updated.
    
    Removed the unnecessary `allIds` field.
    
    Made sure to handle deletions by clearing the range of calendar
    items that the successful fetch covered.

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -310,9 +310,15 @@ export const declineInvite = (id: IInvite["id"]) =>
 
 export const reportBadMerge = () => http.post("/api/v1/report-bad-merge", {})
 
-export const getCalendarRecipeList = (teamID: TeamID, currentDayTs: number) => {
-  const start = toISODateString(startOfWeek(subWeeks(currentDayTs, 1)))
-  const end = toISODateString(endOfWeek(addWeeks(currentDayTs, 1)))
+export function getCalendarRecipeList({
+  teamID,
+  start,
+  end
+}: {
+  teamID: TeamID
+  start: string
+  end: string
+}) {
   const id = teamID === "personal" ? "me" : teamID
   return http.get<ICalRecipe[]>(`/api/v1/t/${id}/calendar/`, {
     params: {
