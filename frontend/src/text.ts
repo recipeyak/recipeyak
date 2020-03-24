b07b85e9b56e6ef58ed8a31d76ba5a90c1a666e2
Steve Dignam <steve@dignam.xyz>
2020-03-24 00:24:07 -0400

    fix: capitalization for units
    We were only doing a singular replace across steps & ingredients instead
    of all occurrences.

--- frontend/src/text.ts
+++ frontend/src/text.ts
@@ -2,5 +2,7 @@
  * Mixing up Tablespoon and teaspoon is harder when they are consistently cased
  */
 export function capitalizeUnits(str: string): string {
-  return str.replace("tablespoon", "Tablespoon").replace("Teaspoon", "teaspoon")
+  return str
+    .replace(/tablespoon/g, "Tablespoon")
+    .replace(/Teaspoon/g, "teaspoon")
 }
