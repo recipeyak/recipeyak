e0a4c1bc892d9b9510e16a3a758b219f6065e529
Steve Dignam <sbdchd@users.noreply.github.com>
2020-03-24 21:33:59 -0400

    fix: add recipe form focus (#557)
    Previously the form was focusing to the first ingredient rather than
    the recipe title.

--- frontend/src/components/AddIngredientForm.tsx
+++ frontend/src/components/AddIngredientForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react"
 
 import { ButtonPrimary, Button } from "@/components/Buttons"
 import { TextInput, CheckBox, selectTarget } from "@/components/Forms"
@@ -36,14 +36,11 @@ function AddIngredientForm({
   error,
   autoFocus = false
 }: IAddIngredientFormProps) {
-  const [isInitialLoad, setInitialLoad] = useState(true)
-
   useEffect(() => {
-    setInitialLoad(false)
-    if (!loading && (autoFocus || !isInitialLoad)) {
+    if (!loading && autoFocus) {
       focusFirstInput()
     }
-  }, [autoFocus, isInitialLoad, loading])
+  }, [autoFocus, loading])
   const handleKeyUp = (e: KeyboardEvent) => {
     if (e.key === "Escape") {
       cancelAddIngredient()
