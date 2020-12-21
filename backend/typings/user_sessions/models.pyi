fb9d7e4a1ae6b5d5f818021311beb049b7b1df70
Steve Dignam <steve@dignam.xyz>
2020-12-20 20:21:44 -0500

    fix stub formatting

--- backend/typings/user_sessions/models.pyi
+++ backend/typings/user_sessions/models.pyi
@@ -1,12 +1,10 @@
 from __future__ import annotations
 
 from datetime import datetime
-from typing import Any
+from typing import Any, Optional
 
 from django.db import models
-
-from core.models import MyUser
-
+from user_sessions.backends.db import SessionStore
 
 class SessionManager(models.Manager["Session"]):
     use_in_migrations = True
@@ -16,21 +14,15 @@ class SessionManager(models.Manager["Session"]):
     ) -> Session: ...
 
 class Session(models.Model):
-
-    session_key = models.CharField(
-        verbose_name="session key", max_length=40, primary_key=True
-    )
-    session_data = models.TextField(verbose_name="session data")
-    expire_date = models.DateTimeField(verbose_name="expiry date", db_index=True)
-    objects = SessionManager()
+    session_key: models.CharField[str]
+    session_data: models.TextField[str]
+    expire_date: models.DateTimeField[datetime]
+    objects: SessionManager
     class Meta:
         verbose_name = "session"
         verbose_name_plural = "sessions"
     def get_decoded(self) -> SessionStore: ...
-    user = models.ForeignKey(MyUser, null=True, on_delete=models.CASCADE)
-    user_agent = models.CharField(null=True, blank=True, max_length=200)
-    last_activity = models.DateTimeField(auto_now=True)
-    ip = models.GenericIPAddressField(null=True, blank=True, verbose_name="IP")
-
-# At bottom to avoid circular import
-from .backends.db import SessionStore  # noqa: E402 isort:skip
+    user: models.ForeignKey[Any]
+    user_agent: models.CharField[Optional[str]]
+    last_activity: models.DateTimeField[datetime]
+    ip: models.GenericIPAddressField[Optional[str]]
