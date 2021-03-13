397f0bf2a3b2b7f72a647eb2f898800d12b77f54
Steve Dignam <steve@dignam.xyz>
2021-03-13 00:22:28 -0500

    add(ingredients): normalizing unicode fractions
    Convert unicode fractions to their asci form.
    
    Also fix minor styling issue with the browse page.

--- frontend/src/text.test.ts
+++ frontend/src/text.test.ts
@@ -1,10 +1,15 @@
-import { capitalizeUnits } from "@/text"
+import { normalizeUnitsFracs } from "@/text"
 
 describe("text", () => {
   test("capitalizeUnits", () => {
     const input = "tablespoon tablespoons Teaspoon Teaspoons"
-    expect(capitalizeUnits(input)).toEqual(
+    expect(normalizeUnitsFracs(input)).toEqual(
       "Tablespoon Tablespoons teaspoon teaspoons",
+    )
+  })
+  test("normalize fractions", () => {
+    expect(normalizeUnitsFracs("1 ½ teaspoon + ¼ tablespoon")).toEqual(
+      "1 1/2 teaspoon + 1/4 tablespoon",
     )
   })
 })
