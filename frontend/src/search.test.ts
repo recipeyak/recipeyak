73d8251f0f6ac6c942a3ce3fd6ac6e683f09b21c
Christopher Dignam <chris@dignam.xyz>
2021-09-03 12:56:42 -0400

    move test

--- frontend/src/search.test.ts
+++ frontend/src/search.test.ts
@@ -1,4 +1,4 @@
-import { getMatchType, matchesQuery, searchRecipes, parseQuery } from "@/search"
+import { getMatchType, matchesQuery, searchRecipes } from "@/search"
 import { IRecipe } from "@/store/reducers/recipes"
 
 function createRecipe(properties?: Partial<IRecipe>): IRecipe {
@@ -53,55 +53,5 @@ describe("search", () => {
       query: "rhubarb",
     }).recipes.map(x => x.recipe.name)
     expect(resultNames).toEqual(["Apple Rhubarb Bars", "Rhubarb Bars"])
-  })
-})
-
-describe("parseQuery", () => {
-  test("simple", () => {
-    expect(parseQuery("tag:chris")).toEqual([{ field: "tag", value: "chris" }])
-  })
-  test("simple", () => {
-    expect(parseQuery("tag:chris")).toEqual([{ field: "tag", value: "chris" }])
-  })
-  test("single quotes", () => {
-    expect(parseQuery("name:'mark bittman'")).toEqual([
-      { field: "name", value: "mark bittman", quoted: true },
-    ])
-  })
-  test("double quotes", () => {
-    expect(parseQuery(`name:"mark bittman"`)).toEqual([
-      { field: "name", value: "mark bittman", quoted: true },
-    ])
-  })
-  test("negative", () => {
-    expect(parseQuery("-tag:chris")).toEqual([
-      { field: "tag", value: "chris", negative: true },
-    ])
-  })
-  test("escape single quote", () => {
-    expect(parseQuery(`-tag:'chri\\'s'`)).toEqual([
-      { field: "tag", value: "chri's", quoted: true, negative: true },
-    ])
-  })
-  test("escape double quote", () => {
-    expect(parseQuery(`-tag:"chri\\"\\"s"`)).toEqual([
-      { field: "tag", value: `chri""s`, quoted: true, negative: true },
-    ])
-  })
-  test("complex", () => {
-    expect(
-      parseQuery(
-        `  tag:chris hello -tag:dessert   name:pie   author:"Christopher Dignam" testing abc "multi word" `,
-      ),
-    ).toEqual([
-      { field: "tag", value: "chris" },
-      { field: null, value: "hello" },
-      { field: "tag", value: "dessert", negative: true },
-      { field: "name", value: "pie" },
-      { field: "author", value: "Christopher Dignam", quoted: true },
-      { field: null, value: "testing" },
-      { field: null, value: "abc" },
-      { field: null, value: "multi word", quoted: true },
-    ])
   })
 })
