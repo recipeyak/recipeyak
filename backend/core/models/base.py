316da46314f9c7c1ea07cc82a9f0c38300292ea1
Christopher Dignam <chris@dignam.xyz>
2021-11-07 22:30:13 -0500

    .

--- backend/core/models/base.py
+++ backend/core/models/base.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from django.utils import timezone
+
 
 class CommonInfo(models.Model):
     """Abstract model for storing common model info"""
 
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(default=timezone.now)
     modified = models.DateTimeField(auto_now=True)
 
     class Meta:
---
+++ backend/core/models/timeline_event.py
@@ -0,0 +1,30 @@
+from enum import Enum
+from typing import TYPE_CHECKING
+
+from django.db import models
+from softdelete.models import SoftDeleteManager, SoftDeleteObject
+
+from core.models.base import CommonInfo
+
+if TYPE_CHECKING:
+    from core.models import User, Recipe  # noqa: F401
+
+
+class TimelineEventKind(Enum):
+    created = "created"
+    archived = "archived"
+    unarchived = "unarchived"
+    deleted = "deleted"
+
+
+class TimelineEvent(CommonInfo, SoftDeleteObject):
+    action = models.CharField(max_length=255)
+    created_by = models.ForeignKey["User"]("User", on_delete=models.CASCADE, null=True)
+    recipe = models.ForeignKey["Recipe"]("Recipe", on_delete=models.CASCADE)
+
+    objects = SoftDeleteManager["TimelineEvent"]()
+
+    class Meta:
+        db_table = "timeline_event"
+        ordering = ["-created"]
+
