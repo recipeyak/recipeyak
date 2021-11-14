a13c6fa5e5806be49fb13303fe2865e3cd72ecf2
Christopher Dignam <chris@dignam.xyz>
2021-11-14 15:47:41 -0500

    ref(ui): remove snapshots (#680)
    These tests aren't valuable to us.

--- frontend/src/components/ListItem.test.tsx
+++ frontend/src/components/ListItem.test.tsx
@@ -1,9 +1,7 @@
 import React from "react"
 import { mount } from "enzyme"
-import renderer from "react-test-renderer"
 import { MemoryRouter } from "react-router"
 import ListItem from "@/components/ListItem"
-import { TestProvider } from "@/testUtils"
 
 describe("<ListItem/>", () => {
   it("renders without crashing", () => {
@@ -12,31 +10,5 @@ describe("<ListItem/>", () => {
         <ListItem id={0} delete={jest.fn()} update={jest.fn()} />
       </MemoryRouter>,
     )
-  })
-
-  test("snap with capitalization", () => {
-    const testCases = [
-      "Add 3 tablespoons of tomato paste. Cook 1 minute.",
-      "Measure 1 teaspoon of rice vinegar.",
-      "Add 1 tablespoon + 2 teaspoons of soy sauce.",
-      "Stir in 1 Teaspoon salt",
-    ]
-
-    testCases.forEach(text => {
-      const tree = renderer
-        .create(
-          <TestProvider>
-            <ListItem
-              id={0}
-              delete={jest.fn()}
-              update={jest.fn()}
-              text={text}
-            />
-          </TestProvider>,
-        )
-        .toJSON()
-
-      expect(tree).toMatchSnapshot()
-    })
   })
 })
