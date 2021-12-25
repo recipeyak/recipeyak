7d23765391fd6dbfc44901e6e3cf2a2dc3052c02
Christopher Dignam <chris@dignam.xyz>
2021-12-25 13:42:10 -0500

    .

--- frontend/src/components/Notes.tsx
+++ frontend/src/components/Notes.tsx
@@ -13,7 +13,7 @@ import orderBy from "lodash/orderBy"
 import Textarea from "react-textarea-autosize"
 import { Markdown } from "@/components/Markdown"
 import { useLocation } from "react-router-dom"
-import { formatAbsoluteDate, formatHumanDateTime } from "@/date"
+import { formatAbsoluteDateTime, formatHumanDateTime } from "@/date"
 import { styled } from "@/theme"
 import * as api from "@/api"
 
@@ -121,7 +121,7 @@ const SmallTime = styled.time`
 
 function NoteTimeStamp({ created }: { readonly created: string }) {
   const date = new Date(created)
-  const prettyDate = formatAbsoluteDate(date, { includeYear: true })
+  const prettyDate = formatAbsoluteDateTime(date, { includeYear: true })
   const humanizedDate = formatHumanDateTime(date)
   return (
     <SmallTime title={prettyDate} dateTime={created} className="text-muted">
