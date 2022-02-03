recipeyak/recipeyak

commit 6b077a64043866fb87227692d4ab5501b7fd9668

ref(ui): cleanup button order, cut nav accent, improve mobile schedule (#692)
- update the ordering of form buttons to have a clear primary action and
  consistent order throughout the application. We were kind of all over
  the place before
- remove the stack overflow style accent color at top of window
  3px vertical space aren't worth it
- update the spacing of days in the mobile schedule view to collapse if
  they don't have any recipes scheduled in them, allowing more packed
  days to expand
- move the `via Team` component into the edit view of the recipe metadata
- update the metadata to wrap in sections so we don't get `5 hours` split
  onto two lines between the `5` and `hours`
- fix hiding the login button on the landing page on small devices
- disable the note button when there is empty text
- cut unused component

frontend/src/components/RecipeTitle.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import uniq from "lodash/uniq"
-import { Button } from "@/components/Buttons"
+import { Button, ButtonPrimary } from "@/components/Buttons"
 import MetaData from "@/components/MetaData"
 import {
   IRecipe,
@@ -12,6 +12,45 @@ import { TextInput } from "@/components/Forms"
 import { hasSelection } from "@/utils/general"
 import { connect } from "react-redux"
 import { Dropdown } from "@/components/RecipeTitleDropdown"
+import Owner from "@/components/Owner"
+import cls from "@/classnames"
+
+function TagEditor({
+  tags,
+  onRemove,
+  onChange,
+  onCreate,
+  newTag,
+  className,
+}: {
+  readonly tags: string[] | undefined
+  readonly newTag: string
+  readonly onRemove: (_: string) => void
+  readonly onChange: (_: React.ChangeEvent<HTMLInputElement>) => void
+  readonly onCreate: (_: React.KeyboardEvent<HTMLInputElement>) => void
+  readonly className?: string
+}) {
+  return (
+    <div className={cls("d-flex mt-2", className)}>
+      <label className="d-flex align-center">tags</label>
+      <div className="ml-2 d-flex align-center">
+        {tags?.map(x => (
+          <span className="tag">
+            {x}{" "}
+            <button className="delete is-small" onClick={() => onRemove(x)} />
+          </span>
+        ))}
+      </div>
+      <TextInput
+        className="ml-2 max-width-200px"
+        placeholder="new tag"
+        value={newTag}
+        onChange={onChange}
+        onKeyDown={onCreate}
+      />
+    </div>
+  )
+}
 
 interface IRecipeTitleProps {
   readonly id: IRecipe["id"]
@@ -133,6 +172,8 @@ class RecipeTitle extends React.Component<
       owner,
       updating,
     } = this.props
+
+    const ownerName = owner.type === "team" ? owner.name : "you"
     return (
       <div>
         <div className="grid-entire-row d-flex justify-space-between p-rel">
@@ -164,11 +205,9 @@ class RecipeTitle extends React.Component<
             <MetaData
               title="click to edit"
               onClick={this.handleEnableEdit}
-              owner={owner}
               author={author}
               source={source}
               servings={servings}
-              recipeId={id}
               time={time}
               tags={tags}
             />
@@ -217,45 +256,37 @@ class RecipeTitle extends React.Component<
                 />
               </label>
             </div>
-            <div className="d-flex mt-2">
-              <label className="d-flex align-center">tags</label>
-              <div className="ml-2 d-flex align-center">
-                {this.state.recipe.tags?.map(x => (
-                  <span className="tag">
-                    {x}{" "}
-                    <button
-                      className="delete is-small"
-                      onClick={() => this.removeTag(x)}
-                    />
-                  </span>
-                ))}
-              </div>
-              <TextInput
-                className="ml-2 max-width-200px"
-                placeholder="new tag"
-                value={this.state.newTag}
-                onChange={this.handleNewTagChange}
-                onKeyDown={this.handleNewTag}
+
+            <div className="d-flex align-center">
+              <TagEditor
+                tags={this.state.recipe.tags}
+                newTag={this.state.newTag}
+                onChange={this.handleInputChange}
+                onCreate={this.handleNewTag}
+                onRemove={this.removeTag}
+                className="mr-2"
               />
+
+              <Owner id={owner.id} name={ownerName} recipeId={id} />
             </div>
 
             <div className="d-flex grid-entire-row align-items-end justify-content-end">
               <Button
+                size="small"
+                className="mr-3"
+                type="button"
+                name="cancel recipe update"
+                onClick={this.toggleEdit}>
+                Cancel
+              </Button>
+              <ButtonPrimary
                 size="small"
                 type="submit"
                 loading={updating}
                 onClick={this.handleSave}
                 name="save recipe">
                 Save
-              </Button>
-              <Button
-                size="small"
-                className="ml-2"
-                type="button"
-                name="cancel recipe update"
-                onClick={this.toggleEdit}>
-                Cancel
-              </Button>
+              </ButtonPrimary>
             </div>
           </div>
         )}

frontend/src/components/AddIngredientForm.tsx
@@ -144,6 +144,26 @@ function AddIngredientForm({
         error={error}
       />
       <div className="field is-grouped">
+        {toggleShowAddSection ? (
+          <div className="flex-grow">
+            <Button
+              size="small"
+              type="button"
+              name="toggle add section"
+              onClick={toggleShowAddSection}>
+              Add Section
+            </Button>
+          </div>
+        ) : null}
+        <p className="control">
+          <Button
+            onClick={cancelAddIngredient}
+            size="small"
+            type="button"
+            name="cancel add ingredient">
+            Cancel
+          </Button>
+        </p>
         <p className="control">
           <ButtonPrimary
             disabled={addDisabled}
@@ -154,27 +174,6 @@ function AddIngredientForm({
             Add
           </ButtonPrimary>
         </p>
-        <p className="control">
-          <Button
-            onClick={cancelAddIngredient}
-            size="small"
-            type="button"
-            name="cancel add ingredient">
-            Cancel
-          </Button>
-        </p>
-        {toggleShowAddSection ? (
-          <div>
-            <span className="mr-3">or</span>
-            <Button
-              size="small"
-              type="button"
-              name="toggle add section"
-              onClick={toggleShowAddSection}>
-              Add Section
-            </Button>
-          </div>
-        ) : null}
       </div>
     </form>
   )

frontend/src/components/AddSectionForm.tsx
@@ -50,15 +50,27 @@ export function AddSectionFormInner({
         />
       </div>
       <div className="field is-grouped">
-        <p className="control">
-          <ButtonPrimary
-            disabled={addDisabled}
-            size="small"
-            type="submit"
-            loading={status === "loading"}>
-            Save
-          </ButtonPrimary>
-        </p>
+        {onRemove ? (
+          <p className="control flex-grow">
+            <Button
+              size="small"
+              type="button"
+              onClick={onRemove}
+              loading={removing === "loading"}>
+              Remove
+            </Button>
+          </p>
+        ) : toggleShowAddSection ? (
+          <p className="control flex-grow">
+            <Button
+              size="small"
+              type="button"
+              name="toggle add section"
+              onClick={toggleShowAddSection}>
+              Add Ingredient
+            </Button>
+          </p>
+        ) : null}
         <p className="control">
           <Button
             onClick={onCancel}
@@ -68,28 +80,15 @@ export function AddSectionFormInner({
             Cancel
           </Button>
         </p>
-        {onRemove ? (
-          <div className="d-flex justify-content-end flex-grow-1">
-            <Button
-              size="small"
-              type="button"
-              onClick={onRemove}
-              loading={removing === "loading"}>
-              Remove
-            </Button>
-          </div>
-        ) : toggleShowAddSection ? (
-          <div>
-            <span className="mr-3">or</span>
-            <Button
-              size="small"
-              type="button"
-              name="toggle add section"
-              onClick={toggleShowAddSection}>
-              Add Ingredient
-            </Button>
-          </div>
-        ) : null}
+        <p className="control">
+          <ButtonPrimary
+            disabled={addDisabled}
+            size="small"
+            type="submit"
+            loading={status === "loading"}>
+            Save
+          </ButtonPrimary>
+        </p>
       </div>
       {status === "failure" && <p>error adding ingredient</p>}
     </form>

frontend/src/components/AddStepForm.tsx
@@ -51,13 +51,22 @@ const AddStepForm = ({
           }}
           value={text}
           className={"my-textarea" + (error ? " is-danger" : "")}
-          placeholder="Add your step here"
+          placeholder="Add a step..."
           name="step"
         />
         {error ? <p className="fs-4 c-danger">A step is required</p> : null}
       </div>
     </div>
-    <div className="field is-grouped">
+    <div className="field is-grouped justify-end">
+      <p className="control">
+        <Button
+          onClick={cancelAddStep}
+          size="small"
+          type="button"
+          name="cancel step">
+          Cancel
+        </Button>
+      </p>
       <p className="control">
         <ButtonPrimary
           size="small"
@@ -67,15 +76,6 @@ const AddStepForm = ({
           loading={loading}>
           Add
         </ButtonPrimary>
-      </p>
-      <p className="control">
-        <Button
-          onClick={cancelAddStep}
-          size="small"
-          type="button"
-          name="cancel step">
-          Cancel
-        </Button>
       </p>
     </div>
   </form>

frontend/src/components/Calendar.tsx
@@ -192,7 +192,7 @@ function Nav({ dayTs, teamID, onPrev, onNext, onCurrent, type }: INavProps) {
   } = useCalendarSettings(teamID)
 
   return (
-    <section className="d-flex justify-space-between align-items-center">
+    <section className="d-flex justify-space-between align-items-center flex-shrink-0">
       <div className="d-flex">
         <CalTitle dayTs={dayTs} />
         <TeamSelect teams={teams} value={teamID} onChange={handleOwnerChange} />
@@ -229,9 +229,6 @@ const CalContainer = styled.div`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
-  @media (max-width: ${p => p.theme.small}) {
-    height: 100%;
-  }
 `
 
 // pull the week from the URL otherwise default to the current time.

frontend/src/components/CalendarDay.tsx
@@ -94,7 +94,6 @@ const CalendarDayContainer = styled.div<ICalendarDayContainerProps>`
   }
   @media (max-width: ${p => p.theme.medium}) {
     width: 100%;
-    min-height: 75px;
   }
 `
 

frontend/src/components/DeadFish.tsx
@@ -1,89 +1,0 @@
-import * as React from "react"
-
-const DeadFish = () => (
-  <svg
-    className="light-text-color"
-    width="274px"
-    height="119px"
-    viewBox="0 0 274 119"
-    version="1.1"
-    xmlns="http://www.w3.org/2000/svg">
-    <g
-      id="skeleton-fish"
-      stroke="none"
-      strokeWidth="1"
-      fill="none"
-      fillRule="evenodd">
-      <g id="dead-fish-v2" transform="translate(-173.000000, -94.000000)">
-        <g id="Group" transform="translate(148.000000, 79.000000)">
-          <ellipse
-            id="Oval-2"
-            strokeWidth="8"
-            cx="76.5"
-            cy="47"
-            rx="7.5"
-            ry="8"
-          />
-          <path
-            d="M245.65863,120 C272.168298,120 293.65863,98.509668 293.65863,72 C293.65863,45.490332 272.168298,24 245.65863,24 C219.148962,24 261.607019,45.490332 261.607019,72 C261.607019,98.509668 219.148962,120 245.65863,120 Z"
-            id="Oval"
-            strokeWidth="10"
-            transform="translate(265.303509, 72.000000) rotate(180.000000) translate(-265.303509, -72.000000) "
-          />
-          <path
-            d="M55.9036848,122 C70.0429197,122 84.7268472,115.88655 95.3521804,106.158756 C104.648297,97.647898 63.8377538,83.3704331 63.8377538,71 C63.8377538,59.8631673 105.821192,52.6121865 98.0375147,44.4682723 C87.2932394,33.2267411 71.2765201,26 55.9036848,26 C29.3940168,26 30.8520733,47.490332 30.8520733,74 C30.8520733,100.509668 29.3940168,122 55.9036848,122 Z"
-            id="Oval"
-            strokeWidth="10"
-            transform="translate(64.918877, 74.000000) rotate(180.000000) translate(-64.918877, -74.000000) "
-          />
-          <rect
-            id="Rectangle"
-            strokeWidth="10"
-            x="104"
-            y="73"
-            width="127"
-            height="2"
-          />
-          <rect
-            id="Rectangle-2"
-            strokeWidth="10"
-            x="122"
-            y="31"
-            width="4"
-            height="86"
-            rx="2"
-          />
-          <rect
-            id="Rectangle-2"
-            strokeWidth="10"
-            x="151"
-            y="20"
-            width="4"
-            height="109"
-            rx="2"
-          />
-          <rect
-            id="Rectangle-2"
-            strokeWidth="10"
-            x="180"
-            y="20"
-            width="4"
-            height="109"
-            rx="2"
-          />
-          <rect
-            id="Rectangle-2"
-            strokeWidth="10"
-            x="209"
-            y="31"
-            width="4"
-            height="86"
-            rx="2"
-          />
-        </g>
-      </g>
-    </g>
-  </svg>
-)
-
-export default DeadFish

frontend/src/components/Ingredient.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 import IngredientView from "@/components/IngredientView"
 import GlobalEvent from "@/components/GlobalEvent"
-import { Button, ButtonLink } from "@/components/Buttons"
+import { Button, ButtonLink, ButtonPrimary } from "@/components/Buttons"
 import { TextInput, selectTarget, CheckBox } from "@/components/Forms"
 import { hasSelection } from "@/utils/general"
 import { useDrop, useDrag } from "react-dnd"
@@ -281,13 +281,15 @@ export function Ingredient(props: {
         <div className="field is-grouped">
           <p className="control">
             <Button
+              onClick={remove}
               size="small"
-              type="submit"
-              name="update"
-              loading={props.updating}>
-              Update
+              loading={props.removing}
+              name="remove">
+              Remove
             </Button>
           </p>
+        </div>
+        <div className="field is-grouped">
           <p className="control">
             <Button
               onClick={handleCancelButton}
@@ -297,16 +299,14 @@ export function Ingredient(props: {
               Cancel
             </Button>
           </p>
-        </div>
-        <div className="field is-grouped">
           <p className="control">
-            <Button
-              onClick={remove}
+            <ButtonPrimary
               size="small"
-              loading={props.removing}
-              name="remove">
-              Remove
-            </Button>
+              type="submit"
+              name="update"
+              loading={props.updating}>
+              Update
+            </ButtonPrimary>
           </p>
         </div>
       </section>

commit abc253d73ab20decafdaaeda5f89192feaf2fa51

fix tag creation
Tag creation was broken in 6b077a64043866fb87227692d4ab5501b7fd9668

frontend/src/components/RecipeTitle.tsx
@@ -18,18 +18,24 @@ import cls from "@/classnames"
 function TagEditor({
   tags,
   onRemove,
-  onChange,
   onCreate,
-  newTag,
+
   className,
 }: {
   readonly tags: string[] | undefined
-  readonly newTag: string
   readonly onRemove: (_: string) => void
-  readonly onChange: (_: React.ChangeEvent<HTMLInputElement>) => void
-  readonly onCreate: (_: React.KeyboardEvent<HTMLInputElement>) => void
+  readonly onCreate: (_: string) => void
   readonly className?: string
 }) {
+  const [newTag, setNewTag] = React.useState('')
+  function handleNewTag(e: React.KeyboardEvent<HTMLInputElement>)  {
+    if (e.key !== "Enter") {
+      return
+    }
+
+    onCreate(newTag)
+    setNewTag('')
+  }
   return (
     <div className={cls("d-flex mt-2", className)}>
       <label className="d-flex align-center">tags</label>
@@ -45,8 +51,8 @@ function TagEditor({
         className="ml-2 max-width-200px"
         placeholder="new tag"
         value={newTag}
-        onChange={onChange}
-        onKeyDown={onCreate}
+        onChange={e => setNewTag(e.target.value)}
+        onKeyDown={handleNewTag}
       />
     </div>
   )
@@ -79,7 +85,6 @@ export interface IRecipeBasic {
 interface IRecipeTitleState {
   readonly show: boolean
   readonly recipe: IRecipeBasic
-  readonly newTag: string
 }
 
 class RecipeTitle extends React.Component<
@@ -89,7 +94,6 @@ class RecipeTitle extends React.Component<
   state: IRecipeTitleState = {
     show: false,
     recipe: {},
-    newTag: "",
   }
 
   componentDidMount() {
@@ -133,21 +137,12 @@ class RecipeTitle extends React.Component<
     this.toggleEdit()
   }
 
-  handleNewTagChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ newTag: e.target.value })
-  }
-
-  handleNewTag = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key !== "Enter") {
-      return
-    }
-    e.persist()
+  handleNewTag = (tag: string) => {
     this.setState(prevState => ({
       recipe: {
         ...prevState.recipe,
-        tags: uniq([...(prevState.recipe.tags ?? []), prevState.newTag]),
+        tags: uniq([...(prevState.recipe.tags ?? []), tag]),
       },
-      newTag: "",
     }))
   }
   removeTag = (tag: string) => {
@@ -260,8 +255,6 @@ class RecipeTitle extends React.Component<
             <div className="d-flex align-center">
               <TagEditor
                 tags={this.state.recipe.tags}
-                newTag={this.state.newTag}
-                onChange={this.handleInputChange}
                 onCreate={this.handleNewTag}
                 onRemove={this.removeTag}
                 className="mr-2"