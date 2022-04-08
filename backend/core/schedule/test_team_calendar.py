195b5b87381c6aae6f3f919bb26726d2bfb952b0
Christopher Dignam <chris@dignam.xyz>
2022-04-08 17:39:44 -0400

    test

--- backend/core/schedule/test_team_calendar.py
+++ backend/core/schedule/test_team_calendar.py
@@ -159,11 +159,18 @@ def test_cal_generate_link_view(
     assert res.json()["calendarLink"] != initial_link, "ensure we changed the link"
 
 
-def test_cal_next_open(client: APIClient, user: User, team: Team) -> None:
+def test_cal_next_open(client: APIClient, user: User, team: Team, empty_team: Team) -> None:
     """
     Check finding the next open day / weekend
     """
     client.force_authenticate(user)
+
+    res = client.get(
+        f"/api/v1/t/{empty_team.pk}/calendar/next_open/",
+        data={"day": "Wednesday", "now": "2022-04-15"},
+    )
+    assert res.status_code == status.HTTP_404_NOT_FOUND, "we shouldn't be able to access other teams"
+
     res = client.get(
         f"/api/v1/t/{team.pk}/calendar/next_open/",
         data={"day": "Wednesday", "now": "2022-04-15"},
