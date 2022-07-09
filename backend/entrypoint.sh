faae07cef4db02ec056b6cf2882a51cc40a2fab0
Steve Dignam <steve@dignam.xyz>
2022-07-09 17:40:57 -0400

    fix(backend): docker entrypoint (#736)
    The entry point is broken now that social auth has been removed.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,7 +7,4 @@ set -eux
 # apply migrations
 /var/app/.venv/bin/python manage.py migrate
 
-# configure server models
-/var/app/.venv/bin/python manage.py shell < set_server.py
-
 PYTHONUNBUFFERED=1 exec /var/app/.venv/bin/gunicorn -w 3 -b 0.0.0.0:8000 core.wsgi --access-logfile - --error-logfile - --capture-output --enable-stdio-inheritance --access-logformat 'request="%(r)s" request_time=%(L)s remote_addr="%(h)s" request_id=%({X-Request-Id}i)s response_id=%({X-Response-Id}i)s method=%(m)s protocol=%(H)s status_code=%(s)s response_length=%(b)s referer="%(f)s" process_id=%(p)s user_agent="%(a)s"'
