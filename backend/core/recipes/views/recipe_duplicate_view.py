3188a3072a86426ca1af07b2d7d69df98a185900
Christopher Dignam <chris@dignam.xyz>
2022-08-26 09:13:54 -0400

    fmt

--- backend/core/recipes/views/recipe_duplicate_view.py
+++ backend/core/recipes/views/recipe_duplicate_view.py
@@ -1,17 +1,12 @@
 from __future__ import annotations
 
-
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from core.models import (
-    user_and_team_recipes,
-)
-from core.recipes.serializers import (
-    RecipeSerializer,
-)
+from core.models import user_and_team_recipes
+from core.recipes.serializers import RecipeSerializer
 from core.request import AuthedRequest
 
 
