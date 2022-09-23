ac64c68fa933b1399b08714a07a22246e0978ac0
Steve Dignam <steve@dignam.xyz>
2022-09-22 20:55:15 -0400

    ref(api): recipe list sentry instrument

--- backend/core/views/recipe_list_view.py
+++ backend/core/views/recipe_list_view.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import collections
 import logging
 from typing import Any, Iterable, Optional
+import sentry_sdk
 
 import advocate
 from django.core.exceptions import ValidationError
@@ -47,152 +48,162 @@ def group_by_recipe_id(x: Iterable[dict[str, Any]]) -> dict[str, list[dict[str, 
 
 
 def recipe_get_view(request: AuthedRequest) -> Response:
-    recipes = {
-        x["id"]: x
-        for x in user_and_team_recipes(request.user).values(
-            "id",
-            "name",
-            "author",
-            "source",
-            "time",
-            "servings",
-            "edits",
-            "modified",
-            "owner_team",
-            "owner_team__name",
-            "owner_user",
-            "created",
-            "archived_at",
-            "tags",
+    with sentry_sdk.start_span(op="db", description="fetch recipes"):
+        recipes = {
+            x["id"]: x
+            for x in user_and_team_recipes(request.user).values(
+                "id",
+                "name",
+                "author",
+                "source",
+                "time",
+                "servings",
+                "edits",
+                "modified",
+                "owner_team",
+                "owner_team__name",
+                "owner_user",
+                "created",
+                "archived_at",
+                "tags",
+            )
+        }
+
+    with sentry_sdk.start_span(op="db", description="fetch ingredients"):
+        ingredients = group_by_recipe_id(
+            Ingredient.objects.filter(recipe_id__in=recipes.keys()).values(
+                "id",
+                "quantity",
+                "name",
+                "description",
+                "position",
+                "recipe_id",
+            )
         )
-    }
 
-    ingredients = group_by_recipe_id(
-        Ingredient.objects.filter(recipe_id__in=recipes.keys()).values(
-            "id",
-            "quantity",
-            "name",
-            "description",
-            "position",
-            "recipe_id",
+    with sentry_sdk.start_span(op="db", description="fetch schedules"):
+        schedule_recipe = dict()
+        for schedule in (
+            ScheduledRecipe.objects.filter(recipe_id__in=recipes.keys())
+            .distinct("recipe_id")
+            .order_by("-recipe_id", "-on")
+            .values("recipe_id", "on")
+        ):
+            schedule_recipe[schedule["recipe_id"]] = schedule["on"]
+
+    with sentry_sdk.start_span(op="db", description="fetch steps"):
+        steps = group_by_recipe_id(
+            Step.objects.filter(recipe_id__in=recipes.keys()).values(
+                "id",
+                "text",
+                "position",
+                "recipe_id",
+            )
         )
-    )
 
-    schedule_recipe = dict()
-    for schedule in (
-        ScheduledRecipe.objects.filter(recipe_id__in=recipes.keys())
-        .distinct("recipe_id")
-        .order_by("-recipe_id", "-on")
-        .values("recipe_id", "on")
-    ):
-        schedule_recipe[schedule["recipe_id"]] = schedule["on"]
+    with sentry_sdk.start_span(op="db", description="fetch sections"):
+        sections = group_by_recipe_id(
+            Section.objects.filter(recipe_id__in=recipes.keys()).values(
+                "id",
+                "title",
+                "position",
+                "recipe_id",
+            )
+        )
 
-    steps = group_by_recipe_id(
-        Step.objects.filter(recipe_id__in=recipes.keys()).values(
+    with sentry_sdk.start_span(op="db", description="fetch notes"):
+        notes = collections.defaultdict(list)
+        note_map = dict()
+        for note in Note.objects.filter(recipe_id__in=recipes.keys()).values(
             "id",
             "text",
-            "position",
+            "modified",
+            "created",
             "recipe_id",
-        )
-    )
+            "last_modified_by",
+            "last_modified_by__email",
+            "last_modified_by__name",
+            "created_by",
+            "created_by__email",
+            "created_by__name",
+        ):
 
-    sections = group_by_recipe_id(
-        Section.objects.filter(recipe_id__in=recipes.keys()).values(
+            for name in ("last_modified_by", "created_by"):
+                if note[name] is not None:
+                    display_name = note.get(f"{name}__name") or note[f"{name}__email"]
+                    email = note[f"{name}__email"]
+                    note[name] = dict(
+                        id=note[name],
+                        email=email,
+                        name=display_name,
+                        avatar_url=get_avatar_url(email),
+                    )
+                else:
+                    note.pop(name, None)
+                note.pop(f"{name}__email", None)
+                note.pop(f"{name}__name", None)
+            note["type"] = "note"
+            note["attachments"] = []
+            note["reactions"] = []
+            notes[note["recipe_id"]].append(note)
+            note_map[note["id"]] = note
+
+    with sentry_sdk.start_span(op="db", description="uploads"):
+        for upload in Upload.objects.filter(note__recipe_id__in=recipes.keys()):
+            note_map[upload.note_id]["attachments"].append(
+                list(serialize_attachments([upload]))[0].dict()
+            )
+    with sentry_sdk.start_span(op="db", description="reactions"):
+        for reaction in Reaction.objects.filter(note__recipe_id__in=recipes.keys()):
+            note_map[reaction.note_id]["reactions"].append(
+                list(serialize_reactions([reaction]))[0].dict()
+            )
+
+    with sentry_sdk.start_span(op="db", description="timeline"):
+        timeline_events = collections.defaultdict(list)
+
+        for event in TimelineEvent.objects.filter(recipe_id__in=recipes.keys()).values(
             "id",
-            "title",
-            "position",
+            "action",
+            "created",
+            "created_by",
+            "created_by__email",
             "recipe_id",
-        )
-    )
-
-    notes = collections.defaultdict(list)
-    note_map = dict()
-    for note in Note.objects.filter(recipe_id__in=recipes.keys()).values(
-        "id",
-        "text",
-        "modified",
-        "created",
-        "recipe_id",
-        "last_modified_by",
-        "last_modified_by__email",
-        "last_modified_by__name",
-        "created_by",
-        "created_by__email",
-        "created_by__name",
-    ):
-
-        for name in ("last_modified_by", "created_by"):
-            if note[name] is not None:
-                display_name = note.get(f"{name}__name") or note[f"{name}__email"]
-                email = note[f"{name}__email"]
-                note[name] = dict(
-                    id=note[name],
+        ):
+            if event["created_by"] is not None:
+                email = event["created_by__email"]
+                event["created_by"] = dict(
+                    id=event["created_by"],
                     email=email,
-                    name=display_name,
                     avatar_url=get_avatar_url(email),
                 )
+            event.pop("created_by__email", None)
+            event["type"] = "recipe"
+            timeline_events[event["recipe_id"]].append(event)
+
+    with sentry_sdk.start_span(op="serialize", description="recipe stuff"):
+        for recipe_id, recipe in recipes.items():
+            if recipe["owner_team"]:
+                recipe["owner"] = dict(
+                    type="team",
+                    id=recipe["owner_team"],
+                    name=recipe["owner_team__name"],
+                )
             else:
-                note.pop(name, None)
-            note.pop(f"{name}__email", None)
-            note.pop(f"{name}__name", None)
-        note["type"] = "note"
-        note["attachments"] = []
-        note["reactions"] = []
-        notes[note["recipe_id"]].append(note)
-        note_map[note["id"]] = note
+                recipe["owner"] = dict(type="user", id=recipe["owner_team"])
 
-    for upload in Upload.objects.filter(note__recipe_id__in=recipes.keys()):
-        note_map[upload.note_id]["attachments"].append(
-            list(serialize_attachments([upload]))[0].dict()
-        )
-    for reaction in Reaction.objects.filter(note__recipe_id__in=recipes.keys()):
-        note_map[reaction.note_id]["reactions"].append(
-            list(serialize_reactions([reaction]))[0].dict()
-        )
+            recipe.pop("owner_user", None)
+            recipe.pop("owner_user", None)
+            recipe.pop("owner_team__name", None)
 
-    timeline_events = collections.defaultdict(list)
+            recipe["last_scheduled_at"] = schedule_recipe.get(recipe_id)
 
-    for event in TimelineEvent.objects.filter(recipe_id__in=recipes.keys()).values(
-        "id",
-        "action",
-        "created",
-        "created_by",
-        "created_by__email",
-        "recipe_id",
-    ):
-        if event["created_by"] is not None:
-            email = event["created_by__email"]
-            event["created_by"] = dict(
-                id=event["created_by"],
-                email=email,
-                avatar_url=get_avatar_url(email),
+            recipe["ingredients"] = ingredients.get(recipe_id) or []
+            recipe["steps"] = steps.get(recipe_id) or []
+            recipe["sections"] = sections.get(recipe_id) or []
+            recipe["timelineItems"] = (notes.get(recipe_id) or []) + (
+                timeline_events.get(recipe_id) or []
             )
-        event.pop("created_by__email", None)
-        event["type"] = "recipe"
-        timeline_events[event["recipe_id"]].append(event)
-
-    for recipe_id, recipe in recipes.items():
-        if recipe["owner_team"]:
-            recipe["owner"] = dict(
-                type="team",
-                id=recipe["owner_team"],
-                name=recipe["owner_team__name"],
-            )
-        else:
-            recipe["owner"] = dict(type="user", id=recipe["owner_team"])
-
-        recipe.pop("owner_user", None)
-        recipe.pop("owner_user", None)
-        recipe.pop("owner_team__name", None)
-
-        recipe["last_scheduled_at"] = schedule_recipe.get(recipe_id)
-
-        recipe["ingredients"] = ingredients.get(recipe_id) or []
-        recipe["steps"] = steps.get(recipe_id) or []
-        recipe["sections"] = sections.get(recipe_id) or []
-        recipe["timelineItems"] = (notes.get(recipe_id) or []) + (
-            timeline_events.get(recipe_id) or []
-        )
 
     return Response(list(recipes.values()))
 
