c9274de528ed236dd5d46475a3379027d6d5bdc3
Christopher Dignam <chris@dignam.xyz>
2022-09-23 22:10:25 -0400

    wip

--- backend/core/ordering.py
+++ backend/core/ordering.py
@@ -1,18 +1,30 @@
+import math
 from typing import Iterable
-
 
 START_CHAR_CODE = 32
 END_CHAR_CODE = 126
 FIRST_POSITION = chr(START_CHAR_CODE + 1)
 
-def comparePositions(firstPos: str, secondPos: str) -> int:
-    return +(firstPos < secondPos) - +(firstPos > secondPos)
+
+def assert_dev(expr):
+    if not expr:
+        raise Exception("Assertion Error")
+
+
+def avg(a: float, b: float) -> int:
+    return math.trunc((a + b) / 2)
+
 
 def iter_char_codes(x: str) -> Iterable[int]:
     for char in x:
         yield ord(char)
 
-def isValidPosition(pos: str) -> bool:
+
+def compare_positions(first_pos: str, second_pos: str) -> int:
+    return (first_pos < second_pos) - ((first_pos > second_pos))
+
+
+def is_valid_position(pos: str) -> bool:
     if pos == "" or ord(pos[-1]) == START_CHAR_CODE:
         return False
     for char_code in iter_char_codes(pos):
@@ -21,56 +33,55 @@ def isValidPosition(pos: str) -> bool:
     return True
 
 
-def positionBefore(pos: str) -> str:
+def position_before(pos: str) -> str:
+    assert_dev(0 != len(pos))
+    i = len(pos) - 1
     for i in reversed(range(len(pos))):
-        cur_char_code = ord(i)
+        cur_char_code = ord(pos[i])
         if cur_char_code > START_CHAR_CODE + 1:
             position = pos[0:i] + chr(cur_char_code - 1)
-            assert isValidPosition(position)
+            assert_dev(is_valid_position(position))
             return position
-    position = pos[0: len(pos) - 1] + chr(START_CHAR_CODE) + chr(END_CHAR_CODE)
-    assert isValidPosition(position)
+    position = pos[0 : len(pos) - 1] + chr(START_CHAR_CODE) + chr(END_CHAR_CODE)
+    assert_dev(is_valid_position(position))
     return position
 
-def positionAfter(pos: str):
-  for i in reversed(range(len(pos))):
-    curCharCode = ord(pos[i])
-    if curCharCode < END_CHAR_CODE:
-      position = pos[0:i] + chr(curCharCode + 1)
-      assert isValidPosition(position)
-      return position
 
-  position = pos + chr(START_CHAR_CODE + 1)
-  assert isValidPosition(position)
-  return position
+def position_after(pos: str) -> str:
+    assert_dev(0 != len(pos))
+    for i in reversed(range(len(pos))):
+        cur_char_code = ord(pos[i])
+        if cur_char_code < END_CHAR_CODE:
+            position = pos[0:i] + chr(cur_char_code + 1)
+            assert_dev(is_valid_position(position))
+            return position
+    position = pos + chr(START_CHAR_CODE + 1)
+    assert_dev(is_valid_position(position))
+    return position
 
 
-export function positionBetween(firstPos: string, secondPos: string) {
-  assertDev(firstPos < secondPos)
-  let flag = false
-  let position = ""
-  const maxLength = Math.max(firstPos.length, secondPos.length)
-  for (let i = 0; i < maxLength; i++) {
-    const lower = i < firstPos.length ? firstPos.charCodeAt(i) : START_CHAR_CODE
-    const upper =
-      i < secondPos.length && !flag ? secondPos.charCodeAt(i) : END_CHAR_CODE
-    if (lower === upper) {
-      position += String.fromCharCode(lower)
-    } else if (upper - lower > 1) {
-      position += String.fromCharCode(avg(lower, upper))
-      flag = false
-      break
-    } else {
-      position += String.fromCharCode(lower)
-      flag = true
-    }
-  }
-
-  if (flag) {
-    position += String.fromCharCode(avg(START_CHAR_CODE, END_CHAR_CODE))
-  }
-  assertDev(firstPos < position)
-  assertDev(position < secondPos)
-  assertDev(isValidPosition(position))
-  return position
-}
+def position_between(first_pos: str, second_pos: str) -> str:
+    assert_dev(first_pos < second_pos)
+    flag = False
+    position = ""
+    max_length = max(len(first_pos), len(second_pos))
+    for i in range(max_length):
+        lower = ord(first_pos[i]) if i < len(first_pos) else START_CHAR_CODE
+        upper = (
+            ord(second_pos[i]) if i < len(second_pos) and not flag else END_CHAR_CODE
+        )
+        if lower == upper:
+            position += chr(lower)
+        elif upper - lower > 1:
+            position += chr(avg(lower, upper))
+            flag = False
+            break
+        else:
+            position += chr(lower)
+            flag = True
+    if flag:
+        position += chr(avg(START_CHAR_CODE, END_CHAR_CODE))
+    assert_dev(first_pos < position)
+    assert_dev(position < second_pos)
+    assert_dev(is_valid_position(position))
+    return position
