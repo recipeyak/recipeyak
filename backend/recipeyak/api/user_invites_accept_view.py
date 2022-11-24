f9ae4f61e9c75053a4e887ca66a1656b94194c1e
Steve Dignam <steve@dignam.xyz>
2022-11-23 22:52:29 -0500

    fix

--- backend/recipeyak/api/user_invites_accept_view.py
+++ backend/recipeyak/api/user_invites_accept_view.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -10,6 +11,6 @@ from recipeyak.api.user_invites_list_view import get_invites
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def user_invites_accept_view(request: AuthedRequest, pk: str) -> Response:
-    invite = get_invites(request.user).get(id=pk)
+    invite = get_object_or_404(get_invites(request.user), id=pk)
     invite.accept()
     return Response({"detail": "accepted invite"}, status=status.HTTP_200_OK)
