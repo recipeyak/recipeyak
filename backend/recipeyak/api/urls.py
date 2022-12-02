be32d1ce089ad31c55bb4181c401a35d0dfacca3
Steve Dignam <steve@dignam.xyz>
2022-12-01 21:00:16 -0500

    ref(api): convert user details to func views (#876)
    slowly removing all the class based views

--- backend/recipeyak/api/urls.py
+++ backend/recipeyak/api/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 from django.urls import path
 
-from recipeyak.api import recipe_bot_detail_view
 from recipeyak.api.calendar_detail_view import calendar_detail_view
 from recipeyak.api.calendar_generate_link_view import generate_link
 from recipeyak.api.calendar_list_view import calendar_list_view
@@ -25,6 +24,9 @@ from recipeyak.api.reactions_view import (
 )
 from recipeyak.api.recently_created_recipes_view import get_recently_created_recipes
 from recipeyak.api.recently_view_recipes_view import get_recently_viewed_recipes
+from recipeyak.api.recipe_bot_detail_view import (
+    receipe_detail_view as recipe_bot_detail_view,
+)
 from recipeyak.api.recipe_copy_view import recipe_copy_view
 from recipeyak.api.recipe_detail_view import receipe_detail_view
 from recipeyak.api.recipe_duplicate_view import recipe_duplicate_view
@@ -48,10 +50,10 @@ from recipeyak.api.team_shopping_list_detail_view import get_shopping_list_view
 from recipeyak.api.timeline_view import get_recipe_timeline
 from recipeyak.api.uploads_detail_view import complete_upload_view
 from recipeyak.api.uploads_list_view import start_upload_view
+from recipeyak.api.user_detail_view import user_detail_view
 from recipeyak.api.user_invites_accept_view import user_invites_accept_view
 from recipeyak.api.user_invites_decline_view import user_invites_decline_view
 from recipeyak.api.user_invites_list_view import user_invites_list_view
-from recipeyak.api.users_detail_view import UserDetailsView
 
 urlpatterns = [
     path("api/v1/t/<team_pk>/members/", team_members_list_view),
@@ -99,12 +101,9 @@ urlpatterns = [
     path("api/v1/t/<team_pk>/shoppinglist/", get_shopping_list_view),
     path("api/v1/upload/", start_upload_view),
     path("api/v1/upload/<int:upload_pk>/complete", complete_upload_view),
-    path("api/v1/user/", UserDetailsView.as_view()),
+    path("api/v1/user/", user_detail_view),
     path("t/<int:team_id>/ical/<str:ical_id>/schedule.ics", get_ical_view),
-    url(
-        r"^recipes/(?P<recipe_pk>[0-9]+)(-.*)?$",
-        recipe_bot_detail_view.receipe_detail_view,
-    ),
+    url(r"^recipes/(?P<recipe_pk>[0-9]+)(-.*)?$", recipe_bot_detail_view),
     url(r"^recipes.(?P<filetype>json|yaml|yml)$", export_recipes_list_view),
     url(
         r"^recipes/(?P<pk>[0-9]+)\.(?P<filetype>json|yaml|yml)$",
