dc49454cf0993fec18e27cee1aa3ff152a8ed8de
Steve Dignam <steve@dignam.xyz>
2022-12-24 20:55:34 -0500

    fix

---
+++ frontend/src/toast.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-restricted-imports
+import toast, { Toaster } from "react-hot-toast"
+
+// Things to improve:
+// - sticky toasts
+// - dismissable toasts
+// - more typescript friendly api, like toast({ message, type: "success" })
+export { toast, Toaster }
