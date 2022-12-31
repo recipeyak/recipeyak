20104f187d7d4018b433c54a23fce7dd9ddf2c62
Steve Dignam <steve@dignam.xyz>
2022-12-31 15:17:27 -0500

    fix lint

--- frontend/src/queries/recipeList.ts
+++ frontend/src/queries/recipeList.ts
@@ -1,4 +1,4 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query"
 
 import { getRecipeList } from "@/api"
 import { useTeamId } from "@/hooks"
