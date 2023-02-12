57a68b91c240e420b55112763093485f72dff84c
Steve Dignam <steve@dignam.xyz>
2023-02-11 23:53:36 -0500

    fix

--- backend/recipeyak/api/recently_view_recipes_view.py
+++ backend/recipeyak/api/recently_view_recipes_view.py
@@ -5,12 +5,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from recipeyak.api.base.request import AuthedRequest
-from recipeyak.models import RecipeView
+from recipeyak.models import RecipeView, get_team
 
 
 @api_view(["GET"])
 @permission_classes((IsAuthenticated,))
 def get_recently_viewed_recipes(request: AuthedRequest) -> Response:
+    team = get_team(request)
     recipes = [
         {
             "id": rv.recipe.id,
@@ -27,6 +28,7 @@ def get_recently_viewed_recipes(request: AuthedRequest) -> Response:
         }
         for rv in RecipeView.objects.select_related("recipe__primary_image")
         .filter(user=request.user)
+        .filter(recipe__team=team)
         .order_by("-last_visited_at")[:6]
     ]
     return Response(recipes)
