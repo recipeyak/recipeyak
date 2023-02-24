75d520838e849103db6c4736db62aec9e009db86
Steve Dignam <steve@dignam.xyz>
2023-02-23 23:46:57 -0500

    cleanup

--- frontend/src/useIntersectionObserver.ts
+++ frontend/src/useIntersectionObserver.ts
@@ -1,9 +1,5 @@
 import { RefObject, useEffect, useState } from "react"
 
-interface Args extends IntersectionObserverInit {
-  freezeOnceVisible?: boolean
-}
-
 // from: https://usehooks-ts.com/react-hook/use-intersection-observer
 export function useIntersectionObserver(
   elementRef: RefObject<Element>,
@@ -12,7 +8,7 @@ export function useIntersectionObserver(
     root = null,
     rootMargin = "0%",
     freezeOnceVisible = true,
-  }: Args,
+  }: IntersectionObserverInit & { freezeOnceVisible?: boolean },
 ): IntersectionObserverEntry | undefined {
   const [entry, setEntry] = useState<IntersectionObserverEntry>()
 
