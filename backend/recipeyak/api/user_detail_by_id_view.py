eda74cd28fab3c06fc58a4fc4db9ea20a60cfabc
Steve Dignam <steve@dignam.xyz>
2023-08-30 22:50:09 -0400

    profile: add scheduled recipe count (#1036)

--- backend/recipeyak/api/user_detail_by_id_view.py
+++ backend/recipeyak/api/user_detail_by_id_view.py
@@ -10,6 +10,7 @@ from rest_framework.response import Response
 from recipeyak.api.base.request import AuthedRequest
 from recipeyak.api.unwrap import unwrap
 from recipeyak.models.note import Note
+from recipeyak.models.scheduled_recipe import ScheduledRecipe
 from recipeyak.models.upload import Upload
 from recipeyak.models.user import User
 
@@ -17,6 +18,7 @@ from recipeyak.models.user import User
 class UserDetailByIdStats(pydantic.BaseModel):
     recipesAdd: int
     recipesArchived: int
+    scheduled: int
     comments: int
     photos: int
     primaryPhotos: int
@@ -108,6 +110,10 @@ WHERE
     return primary_images_count  # type: ignore[no-any-return]
 
 
+def get_scheduled_count(*, user_id: str) -> int:
+    return ScheduledRecipe.objects.filter(user_id=user_id).count()
+
+
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def user_detail_by_id_view(request: AuthedRequest, user_id: str) -> Response:
@@ -115,6 +121,7 @@ def user_detail_by_id_view(request: AuthedRequest, user_id: str) -> Response:
     recipes_added_count = get_recipes_added_count(user_id=user_id)
     recipes_archived_count = get_recipes_archived_count(user_id=user_id)
     comments_count = get_comments_count(user_id=user_id)
+    scheduled_count = get_scheduled_count(user_id=user_id)
     photos_count = get_photos_count(user_id=user_id)
     primary_photos_count = get_primary_photos_count(user_id=user_id)
     return Response(
@@ -128,6 +135,7 @@ def user_detail_by_id_view(request: AuthedRequest, user_id: str) -> Response:
                 recipesAdd=recipes_added_count,
                 recipesArchived=recipes_archived_count,
                 comments=comments_count,
+                scheduled=scheduled_count,
                 photos=photos_count,
                 primaryPhotos=primary_photos_count,
             ),
--- frontend/src/pages/profile/Profile.page.tsx
+++ frontend/src/pages/profile/Profile.page.tsx
@@ -48,6 +48,27 @@ const AppIcon = () => (
   </svg>
 )
 
+const CalendarIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M21 13V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h8" />
+    <line x1="16" x2="16" y1="2" y2="6" />
+    <line x1="8" x2="8" y1="2" y2="6" />
+    <line x1="3" x2="21" y1="10" y2="10" />
+    <line x1="19" x2="19" y1="16" y2="22" />
+    <line x1="16" x2="22" y1="19" y2="19" />
+  </svg>
+)
+
 const PlantIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -141,6 +162,7 @@ function Profile(props: RouteComponentProps<{ userId: string }>) {
     [userInfo.data.stats.recipesAdd, "Recipes Added", PlantIcon],
     [userInfo.data.stats.recipesArchived, "Recipes Archived", TrashIcon],
     [userInfo.data.stats.comments, "Comments", MessageIcon],
+    [userInfo.data.stats.scheduled, "Scheduled", CalendarIcon],
     [userInfo.data.stats.photos, "Photos", AppIcon],
     [userInfo.data.stats.primaryPhotos, "Primary Photos", TrophyIcon],
   ] as const
@@ -170,7 +192,7 @@ function Profile(props: RouteComponentProps<{ userId: string }>) {
 
       <span className="fs-6">Stats</span>
 
-      <Box dir="row" align="start" wrap gap={4} style={{ fontSize: 18 }}>
+      <Box dir="row" align="start" wrap gap={2}>
         {allStats.map(([value, name, Icon]) => {
           return (
             <Box
