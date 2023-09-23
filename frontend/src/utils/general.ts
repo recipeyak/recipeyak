137576f783386137cf3407b9850b0e6fe8dff1b7
Christopher Dignam <chris@dignam.xyz>
2023-09-22 22:27:33 -0400

    undo

--- frontend/src/utils/general.ts
+++ frontend/src/utils/general.ts
@@ -1,11 +1,3 @@
 export function notUndefined<T>(x: T | undefined): x is T {
   return x != null
 }
-/**
- * Throws if value is undefined
- */
-export function assertNotNullish<T>(x: T | undefined): asserts x is T {
-  if (x == null) {
-    throw Error("Expected value to be not-nullish")
-  }
-}
