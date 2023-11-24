23d050f26897305a8ab044f3ad6e43e01dcd0630
Christopher Dignam <chris@dignam.xyz>
2023-11-24 11:58:48 -0500

    refactor modal

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -13,7 +13,7 @@ interface IModalProps {
   readonly title: string
 }
 
-const ModalContainer = styled.div`
+const ModalPositioner = styled.div`
   align-items: center;
   display: none;
   justify-content: center;
@@ -27,17 +27,10 @@ const ModalContainer = styled.div`
   inset: 0;
 `
 
-const ModalContent = styled.div`
-  margin: 0 20px;
-  max-height: calc(100vh - 160px);
-  overflow: auto;
+const ModalContainer = styled.div`
   position: relative;
-  width: 100%;
-  @media screen and (min-width: 769px), print {
-    margin: 0 auto;
-    max-height: calc(100vh - 40px);
-    width: 640px;
-  }
+  margin-top: 8vh;
+  width: 400px;
 `
 
 const ModalBackground = styled.div`
@@ -59,21 +52,12 @@ export function Modal({ show, content, onClose, title }: IModalProps) {
     return null
   }
   return (
-    <ModalContainer
+    <ModalPositioner
       ref={ref}
       style={{ display: show ? "flex" : undefined, alignItems: "flex-start" }}
     >
       <ModalBackground onClick={onClose} />
-      <ModalContent
-        style={{
-          maxWidth: 400,
-          width: 400,
-          overflowY: "hidden",
-          margin: 20,
-          marginTop: "8vh",
-          fontSize: 14,
-        }}
-      >
+      <ModalContainer>
         <BorderBox display="flex" flexDirection="column" h={100}>
           <Box space="between" mb={2}>
             <h1 className="fs-14px fw-500">{title}</h1>
@@ -81,7 +65,7 @@ export function Modal({ show, content, onClose, title }: IModalProps) {
           </Box>
           {content}
         </BorderBox>
-      </ModalContent>
-    </ModalContainer>
+      </ModalContainer>
+    </ModalPositioner>
   )
 }
