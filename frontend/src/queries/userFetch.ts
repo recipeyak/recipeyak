5116e4612508fe9e75b2d5ef605091646227c2fc
Christopher Dignam <chris@dignam.xyz>
2023-11-25 16:19:55 -0500

    separate cache for auth

--- frontend/src/queries/userFetch.ts
+++ frontend/src/queries/userFetch.ts
@@ -1,4 +1,5 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { experimental_createPersister } from "@tanstack/react-query-persist-client"
 
 import { login } from "@/auth"
 import { http } from "@/http"
@@ -29,5 +30,10 @@ export function useUserFetch() {
       login(res, queryClient)
       return res
     },
+    gcTime: Infinity,
+    persister: experimental_createPersister({
+      storage: localStorage,
+      prefix: "user-fetch",
+    }),
   })
 }
