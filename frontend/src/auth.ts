b755323957455e06226f83790bed839fc5c5b3ae
Steve Dignam <steve@dignam.xyz>
2023-11-26 20:21:51 -0500

    fix

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -2,13 +2,22 @@ import * as Sentry from "@sentry/react"
 import { QueryClient } from "@tanstack/react-query"
 
 import { IUser } from "@/queries/userFetch"
+import { removeItem, setItem } from "@/storage"
 import { themeSet } from "@/theme"
+import { useLocalStorage } from "@/useLocalStorage"
+
+const LOGGED_IN_CACHE_KEY = "loggedIn"
+
+export function useIsLoggedIn(): boolean {
+  const value = useLocalStorage(LOGGED_IN_CACHE_KEY)
+  return value != null
+}
 
 export function logout(queryClient: QueryClient) {
   queryClient.clear()
   Sentry.setUser(null)
   themeSet("light")
-  localStorage.removeItem("loggedIn")
+  removeItem(LOGGED_IN_CACHE_KEY)
 }
 
 export function login(user: IUser, queryClient: QueryClient) {
@@ -20,5 +29,5 @@ export function login(user: IUser, queryClient: QueryClient) {
   queryClient.setQueryData<IUser>(["user-detail"], () => {
     return user
   })
-  localStorage.setItem("loggedIn", "1")
+  setItem(LOGGED_IN_CACHE_KEY, "1")
 }
