189027fe1c265cc4d04234e205c1a09c84768aa6
Steve Dignam <steve@dignam.xyz>
2023-12-14 21:54:57 -0500

    e2e: add some playwright tests
    we're not running them in CI since they use screenshots and are extra
    flakey but they could be useful when making changes to
    styling/components/etc

---
+++ e2e/tests/page-login.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+import { login } from "./authUtils";
+
+test("login page", async ({ page }, testInfo) => {
+  await page.goto("http://localhost:5173/login");
+  await expect(page.getByRole("link", { name: "Login" })).toBeVisible();
+
+  await expect(page).toHaveScreenshot();
+
+  await login(page, testInfo);
+});
---
+++ e2e/tests/page-recipe-detail.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from "@playwright/test";
+import { login } from "./authUtils";
+
+test("recipe detail page", async ({ page }, testInfo) => {
+  await login(page);
+  await page.goto(
+    "http://localhost:5173/recipes/618-cumin-and-cashew-yogurt-rice"
+  );
+
+  await expect(page.getByRole("heading", { name: "Ingredients" })).toBeVisible({
+    timeout: 10000,
+  });
+
+  await expect(page).toHaveScreenshot();
+});
+
+test("schedule modal", async ({ page }, testInfo) => {
+  await login(page);
+  await page.goto(
+    "http://localhost:5173/recipes/618-cumin-and-cashew-yogurt-rice"
+  );
+  await page.getByRole("button", { name: "Actions" }).click();
+  await page.getByRole("button", { name: "Schedule" }).click();
+
+  await expect(page).toHaveScreenshot();
+
+  await page.getByTestId("close modal").click();
+});
+
+test("actions > copy ingredients", async ({ page }, testInfo) => {
+  await login(page);
+  await page.goto(
+    "http://localhost:5173/recipes/618-cumin-and-cashew-yogurt-rice"
+  );
+
+  await page.getByRole("button", { name: "Actions" }).click();
+
+  await expect(page).toHaveScreenshot();
+
+  await page.getByRole("button", { name: "Copy Ingredients" }).click();
+  await expect(page.getByText("Copied ingredients to")).toBeVisible();
+
+  await expect(page).toHaveScreenshot();
+});
