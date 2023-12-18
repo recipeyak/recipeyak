1fbb885ce63ae62c38311f3d4159c11d76152684
Christopher Dignam <chris@dignam.xyz>
2023-12-18 17:01:24 -0500

    fix calendar item border (#1144)

--- frontend/src/pages/recipe-list/RecipeItem.tsx
+++ frontend/src/pages/recipe-list/RecipeItem.tsx
@@ -33,7 +33,7 @@ const Card = forwardRef(
     ref: ForwardedRef<HTMLDivElement>,
   ) => {
     const className = clx(
-      "flex flex-col border border-solid border-[var(--color-background-calendar-day)] bg-[var(--color-background-card)]",
+      "flex flex-col rounded-[6px] border border-solid border-[var(--color-border)] bg-[var(--color-background-card)]",
       isDragging != null && "cursor-move",
       isDragging ? "opacity-50" : "opacity-100",
     )
