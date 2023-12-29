de9c10a753bc6b4047d99345e298d7a1f61fe338
Steve Dignam <steve@dignam.xyz>
2023-12-28 20:16:41 -0500

    fix

--- backend/recipeyak/api/ical_retrieve_view_test.py
+++ backend/recipeyak/api/ical_retrieve_view_test.py
@@ -6,7 +6,7 @@ import pytest
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from recipeyak.api.ical_detail_view import to_ical_time
+from recipeyak.api.ical_retrieve_view import to_ical_time
 from recipeyak.models import Recipe, ScheduledRecipe, Team, User, get_random_ical_id
 from recipeyak.models.membership import Membership
 
