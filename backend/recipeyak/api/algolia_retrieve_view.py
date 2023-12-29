053fd6a94605262452fc9e2d6d840ce622479c87
Steve Dignam <steve@dignam.xyz>
2023-12-29 16:06:42 -0500

    api,ui: algolia cleanup

--- backend/recipeyak/api/algolia_retrieve_view.py
+++ backend/recipeyak/api/algolia_retrieve_view.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import asyncio
-
 from algoliasearch.search_client import SearchClient
 from django.http import JsonResponse
 
@@ -11,18 +9,15 @@ from recipeyak.api.base.request import AuthedHttpRequest
 from recipeyak.api.team_update_view import get_teams
 
 
-async def get_token(team_ids: list[int]) -> str:
-    async with SearchClient.create(
-        app_id=config.ALGOLIA_APPLICATION_ID, api_key=config.ALGOLIA_ADMIN_API_KEY
-    ) as client:
-        # https://www.algolia.com/doc/api-reference/api-methods/generate-secured-api-key/
-        return client.generate_secured_api_key(
-            config.ALGOLIA_SEARCH_ONLY_API_KEY,
-            {
-                "restrictIndices": "recipes",
-                "filters": " OR ".join(f"team_id:{team_id}" for team_id in team_ids),
-            },
-        )
+def get_token(team_ids: list[int]) -> str:
+    # https://www.algolia.com/doc/api-reference/api-methods/generate-secured-api-key/
+    return SearchClient.generate_secured_api_key(
+        config.ALGOLIA_SEARCH_ONLY_API_KEY,
+        {
+            "restrictIndices": "recipes",
+            "filters": " OR ".join(f"team_id:{team_id}" for team_id in team_ids),
+        },
+    )
 
 
 @endpoint()
@@ -32,7 +27,7 @@ def algolia_retrieve_view(request: AuthedHttpRequest) -> JsonResponse:
     return JsonResponse(
         {
             "app_id": config.ALGOLIA_APPLICATION_ID,
-            "api_key": asyncio.run(get_token(team_ids=team_ids)),
+            "api_key": get_token(team_ids=team_ids),
         },
         status=200,
     )
