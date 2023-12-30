a4a9610189e12b97f84ab96ccc4a73dc158dfb08
Christopher Dignam <chris@dignam.xyz>
2023-12-30 16:21:43 -0500

    cache algolia client instance (#1218)

--- frontend/src/queries/useSearchClient.ts
+++ frontend/src/queries/useSearchClient.ts
@@ -1,15 +1,24 @@
 import algoliasearch, { SearchClient } from "algoliasearch/lite"
-import { useMemo } from "react"
 
 import { useAlgoliaApiKey } from "@/queries/useAlgoliaApiKey"
 
+const cache = new Map<string, SearchClient>()
+
+/**
+ * Keep a single stable client across function calls.
+ */
 export function useSearchClient(): SearchClient | null {
   const apiKey = useAlgoliaApiKey()
+  if (apiKey.data?.api_key == null) {
+    return null
+  }
 
-  return useMemo(() => {
-    if (apiKey.data?.api_key == null) {
-      return null
-    }
-    return algoliasearch(apiKey.data.app_id, apiKey.data.api_key)
-  }, [apiKey.data?.api_key, apiKey.data?.app_id])
+  const key = apiKey.data.api_key + apiKey.data.app_id
+  const client = cache.get(key)
+  if (client) {
+    return client
+  }
+  const newClient = algoliasearch(apiKey.data.app_id, apiKey.data.api_key)
+  cache.set(key, newClient)
+  return newClient
 }
