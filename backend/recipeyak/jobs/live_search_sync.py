4ac6262e390c970b4b4002fb963c7857a4d113d2
Christopher Dignam <chris@dignam.xyz>
2023-12-30 17:14:49 -0500

    schedule count all time (#1221)

--- backend/recipeyak/jobs/live_search_sync.py
+++ backend/recipeyak/jobs/live_search_sync.py
@@ -73,7 +73,14 @@ from recipe_index_queue
                         core_scheduledrecipe.recipe_id = core_recipe.id
                         and core_scheduledrecipe.on > (now() - '1.5 years'::interval)
                         and core_scheduledrecipe.on < now()
-                        and core_scheduledrecipe.recipe_id = core_recipe.id
+                ),
+                'scheduled_count_all_time', (
+                    SELECT
+                        count(*)
+                    FROM
+                        core_scheduledrecipe
+                    WHERE
+                        core_scheduledrecipe.recipe_id = core_recipe.id
                 ),
                 'ingredients', (
                     SELECT
