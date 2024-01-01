e903f6ccbaef0a2abd457ed99de5b4a3c7ae6ac6
Steve Dignam <steve@dignam.xyz>
2024-01-01 16:15:10 -0500

    ui,api: show creator/importer of recipe in recently created

--- frontend/src/queries/recentlyCreatedRecipesList.ts
+++ frontend/src/queries/recentlyCreatedRecipesList.ts
@@ -1,37 +1,34 @@
 import { useQuery } from "@tanstack/react-query"
-import * as t from "io-ts"
 
 import { http } from "@/http"
-import { unwrapEither } from "@/query"
+import { unwrapResult } from "@/query"
 import { useTeamId } from "@/useTeamId"
 
 function recentlyCreatedRecipes() {
-  return http.request({
-    method: "GET",
-    url: "/api/v1/recipes/recently_created",
-    shape: t.array(
-      t.type({
-        id: t.number,
-        name: t.string,
-        author: t.union([t.string, t.null]),
-        archivedAt: t.union([t.string, t.null]),
-        primaryImage: t.union([
-          t.type({
-            id: t.number,
-            url: t.string,
-            backgroundUrl: t.union([t.string, t.null]),
-          }),
-          t.null,
-        ]),
-      }),
-    ),
-  })
+  return http.get<
+    Array<{
+      id: number
+      name: string
+      author: string | null
+      archivedAt: string | null
+      primaryImage: {
+        id: number
+        url: string
+        backgroundUrl: string | null
+      } | null
+      createdBy: {
+        id: number
+        name: string
+        avatarUrl: string | null
+      } | null
+    }>
+  >("/api/v1/recipes/recently_created")
 }
 
 export function useRecentlyCreatedRecipesList() {
   const teamId = useTeamId()
   return useQuery({
     queryKey: [teamId, "recently-created-recipes"],
-    queryFn: () => recentlyCreatedRecipes().then(unwrapEither),
+    queryFn: () => recentlyCreatedRecipes().then(unwrapResult),
   })
 }
