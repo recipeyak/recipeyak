c7c517a1fad6708d12d6239b604c3aa1b44020d7
Christopher Dignam <chris@dignam.xyz>
2024-01-10 08:25:20 -0500

    .

--- frontend/src/components/AlgoliaProvider.tsx
+++ frontend/src/components/AlgoliaProvider.tsx
@@ -6,15 +6,6 @@ import { useTeamId } from "@/useTeamId"
 
 export const AlgoliaContext = createContext<SearchClient | null>(null)
 
-// https://github.com/algolia/algoliasearch-client-javascript/blob/5a2a128861b0e037bfb69e47a3ba0b88e9798386/packages/client-search/src/methods/client/getSecuredApiKeyRemainingValidity.ts#L3
-function validUntilSeconds(key: string): number | null {
-  const match = atob(key).match(/validUntil=(\d+)/)
-  if (!match) {
-    return null
-  }
-  return parseInt(match[1], 10) - Math.round(new Date().getTime() / 1000)
-}
-
 export function AlgoliaProvider({ children }: { children: React.ReactNode }) {
   const teamId = useTeamId()
   const res = useAlgoliaClient(teamId)
