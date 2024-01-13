fd1182ac65f39e2f589fa2f807d6bca3ef9d065e
Christopher Dignam <chris@dignam.xyz>
2024-01-13 11:32:40 -0500

    support dragging and recipe URL onto calendar page (#1286)

--- frontend/src/pages/schedule/CalendarDay.tsx
+++ frontend/src/pages/schedule/CalendarDay.tsx
@@ -4,7 +4,7 @@ import format from "date-fns/format"
 import isFirstDayOfMonth from "date-fns/isFirstDayOfMonth"
 import isWithinInterval from "date-fns/isWithinInterval"
 import startOfDay from "date-fns/startOfDay"
-import { sortBy } from "lodash-es"
+import { sortBy, startCase } from "lodash-es"
 import { useState } from "react"
 import { useDrop } from "react-dnd"
 import { useLocation } from "react-router"
@@ -116,6 +116,7 @@ export function CalendarDay({
       }}
       onDrop={(e) => {
         const recipe = e.dataTransfer.getData("recipeyak/recipe")
+        const uriList = e.dataTransfer.getData("text/uri-list")
         if (recipe) {
           // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
           const parsedRecipe: { id: number; name: string } = JSON.parse(recipe)
@@ -124,6 +125,27 @@ export function CalendarDay({
             on: date,
             recipeName: parsedRecipe.name,
           })
+          return
+        }
+        // support dragging any recipe URL into the calendar page.
+        if (uriList) {
+          const uri = new URL(uriList)
+          if (uri.origin === window.location.origin) {
+            const match = /\/recipes\/(\d+)-?(.*)/gm.exec(uri.pathname)
+            const recipeId = match?.[1]
+            // For URLs, we try to extract the name from the slug to provide a
+            // better temporary name in the calendar. Once the recipe is
+            // scheduled, we'll have the correct name.
+            const recipeSlug = startCase(match?.[2].replace("-", " ") || "")
+
+            if (recipeId) {
+              scheduledRecipeCreate.mutate({
+                recipeID: parseInt(recipeId, 10),
+                on: date,
+                recipeName: recipeSlug || "(new recipe)",
+              })
+            }
+          }
         }
       }}
       className={clx(
