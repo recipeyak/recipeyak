d0089da24274e464f65d79d1615c4676c3ef40c1
Christopher Dignam <chris@dignam.xyz>
2024-01-21 20:12:00 -0500

    fix svg warning (#1345)

--- frontend/src/pages/schedule/Calendar.tsx
+++ frontend/src/pages/schedule/Calendar.tsx
@@ -100,9 +100,9 @@ const ShopIcon = () => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <path d="m3 17 2 2 4-4" />
     <path d="m3 7 2 2 4-4" />
@@ -120,9 +120,9 @@ const Plus = () => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <path d="M5 12h14" />
     <path d="M12 5v14" />
