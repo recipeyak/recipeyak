329a79e8c7c9ee4afb9f7cc7715cb3db98c0c9b4
Steve Dignam <steve@dignam.xyz>
2024-01-27 15:16:17 -0500

    ui: fix calculating width of page (#1359)

--- frontend/src/pages/user-comments/UserUploads.page.tsx
+++ frontend/src/pages/user-comments/UserUploads.page.tsx
@@ -1,4 +1,4 @@
-import { useEffect as useLayoutEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { RouteComponentProps } from "react-router"
 import { Link } from "react-router-dom"
 
@@ -6,19 +6,29 @@ import { Image } from "@/components/Image"
 import { Loader } from "@/components/Loader"
 import { NavPage } from "@/components/Page"
 import { ProfilePageContainer } from "@/pages/profile/Profile.page"
+import { ResponseFromUse } from "@/queries/queryUtilTypes"
 import { useUserUploadsList } from "@/queries/userUploadsList"
 import { recipeURL } from "@/urls"
+
+// NOTE: need to keep in sync with the ProfilePageContainer
+const MAX_WIDTH = 700
+// NOTE: need to keep this in sync with the padding in the ProfilePageContainer
+const PADDING_X = 12 * 2
+function getPageWidth() {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return Math.min(window.visualViewport!.width - PADDING_X, MAX_WIDTH)
+}
 
 export function UserUploadsPage(
   props: RouteComponentProps<{ userId: string }>,
 ) {
   const uploads = useUserUploadsList({ id: props.match.params.userId })
   const ref = useRef<HTMLSpanElement>(null)
-  const maxWidth = 700
-  const [innerPageWidth, setInnerPageWidth] = useState(maxWidth)
-  useLayoutEffect(() => {
+
+  const [innerPageWidth, setInnerPageWidth] = useState(getPageWidth)
+  useEffect(() => {
     const handler = () => {
-      setInnerPageWidth(ref.current?.clientWidth ?? maxWidth)
+      setInnerPageWidth(getPageWidth())
     }
     window.addEventListener("resize", handler)
     return () => {
@@ -42,9 +52,23 @@ export function UserUploadsPage(
     )
   }
 
-  const GAP = 2
-  const MIN_IMAGE_SIZE = 100
+  return (
+    <ProfilePageContainer userId={props.match.params.userId}>
+      <span className="text-2xl" ref={ref}>
+        Photos
+      </span>
+      <ImageGrid
+        uploads={uploads.data.uploads}
+        innerPageWidth={innerPageWidth}
+      />
+    </ProfilePageContainer>
+  )
+}
 
+// NOTE: keep this in sync with css gap
+const GAP = 2
+const MIN_IMAGE_SIZE = 100
+function getImageSize(innerPageWidth: number) {
   // 7 = 700 / 100
   const initialRowCount = innerPageWidth / MIN_IMAGE_SIZE
   // 688 = 700 - (7 - 1) * 2
@@ -54,32 +78,36 @@ export function UserUploadsPage(
   // 690 =
   const widthForImages = innerPageWidth - (maxItemsPerRow - 1) * GAP
   // 115 = 690 / 6
-  const imageSize = widthForImages / maxItemsPerRow
+  return widthForImages / maxItemsPerRow
+}
 
+function ImageGrid({
+  uploads,
+  innerPageWidth,
+}: {
+  uploads: ResponseFromUse<typeof useUserUploadsList>["uploads"]
+  innerPageWidth: number
+}) {
+  const imageSize = getImageSize(innerPageWidth)
   return (
-    <ProfilePageContainer userId={props.match.params.userId}>
-      <span className="text-2xl" ref={ref}>
-        Photos
-      </span>
-      <div className="flex flex-wrap gap-[2px]">
-        {uploads.data?.uploads.map((upload) => (
-          <Link
-            key={upload.id}
-            className="h-[--image-size] w-[--image-size]"
-            // eslint-disable-next-line no-restricted-syntax
-            style={{
-              // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-              ["--image-size" as string]: `${imageSize}px`,
-            }}
-            to={
-              recipeURL(upload.note.recipe.id, upload.note.recipe.name) +
-              `#note-${upload.note.id}`
-            }
-          >
-            <Image imgixFmt="small" sources={upload} />
-          </Link>
-        ))}
-      </div>
-    </ProfilePageContainer>
+    <div className="flex flex-wrap gap-[2px]">
+      {uploads.map((upload) => (
+        <Link
+          key={upload.id}
+          className="h-[--image-size] w-[--image-size]"
+          // eslint-disable-next-line no-restricted-syntax
+          style={{
+            // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+            ["--image-size" as string]: `${imageSize}px`,
+          }}
+          to={
+            recipeURL(upload.note.recipe.id, upload.note.recipe.name) +
+            `#note-${upload.note.id}`
+          }
+        >
+          <Image imgixFmt="small" sources={upload} />
+        </Link>
+      ))}
+    </div>
   )
 }
