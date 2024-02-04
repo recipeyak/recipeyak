f87541f8ca8ba4ddad3e3922123fadb9f8b881d5
Christopher Dignam <chris@dignam.xyz>
2024-02-04 00:00:46 -0500

    improve performance of recently created view
    also fix bug in determining recipe author

--- backend/recipeyak/api/recipe_recently_created_view.py
+++ backend/recipeyak/api/recipe_recently_created_view.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import json
 from datetime import datetime
 
 import pydantic
+from django.db import connection
 
 from recipeyak.api.base.decorators import endpoint
 from recipeyak.api.base.request import AuthedHttpRequest
@@ -38,41 +40,49 @@ def recipe_recently_created_view(
 ) -> JsonResponse[list[RecipeRecentlyCreatedViewResponse]]:
     team = get_team(request.user)
     recipes = list[RecipeRecentlyCreatedViewResponse]()
-    for r in filter_recipes(team=team).order_by("-created")[:6]:
-        # A little hacky to appease Django ORM's prefetch related caching logic
-        create_event = next(
-            (x for x in r.timelineevent_set.all() if x.created_by is not None), None
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+ select json_object(
+    'id': id,
+    'name': name,
+    'author': author,
+    'archivedAt': archived_at,
+    'primaryImage':  (
+        select json_object(
+            'id': core_upload.id,
+            'url': 'https://images-cdn.recipeyak.com/' || "key" ,
+            'backgroundUrl': "background_url"
         )
-        if create_event is not None and create_event.created_by is not None:
-            created_by = CreatedByResponse(
-                id=create_event.created_by.id,
-                name=create_event.created_by.name or create_event.created_by.email,
-                avatarUrl=get_avatar_url(
-                    email=create_event.created_by.email,
-                    profile_upload_key=create_event.created_by.profile_upload.key
-                    if create_event.created_by.profile_upload is not None
-                    else None,
+        from core_upload
+        where core_upload.id = core_recipe.primary_image_id
+    ),
+    'createdBy': (
+        select json_object(
+            'id': core_myuser.id,
+            'name': COALESCE(core_myuser.name, core_myuser.email),
+            'avatarUrl': COALESCE((
+                select 'https://images-cdn.recipeyak.com/' || key 
+                from core_upload
+                where core_myuser.profile_upload_id = core_upload.id
                 ),
-            )
-        else:
-            created_by = None
-        primary_image = (
-            PrimaryImageResponse(
-                id=r.primary_image.id,
-                url=r.primary_image.public_url(),
-                backgroundUrl=r.primary_image.background_url,
-            )
-            if r.primary_image is not None
-            else None
-        )
-        recipes.append(
-            RecipeRecentlyCreatedViewResponse(
-                id=r.id,
-                name=r.name,
-                author=r.author,
-                archivedAt=r.archived_at,
-                primaryImage=primary_image,
-                createdBy=created_by,
+                '/avatar/' || md5(core_myuser.email) || '?d=identicon&r=g'
             )
         )
+        from core_myuser
+        join timeline_event on timeline_event.recipe_id = core_recipe.id
+            and timeline_event.created_by_id = core_myuser.id
+            and timeline_event."action" = 'created'
+    )
+) recipe
+from core_recipe
+where team_id = %(team_id)s 
+order by created desc
+limit 6;
+""",
+            {"team_id": team.id},
+        )
+        rows = cursor.fetchall()
+        for row in rows:
+            recipes.append(RecipeRecentlyCreatedViewResponse.model_validate(row[0]))
     return JsonResponse(recipes)
