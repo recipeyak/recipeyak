c3312390156f78a979896601928a22bf8497410d
Steve Dignam <steve@dignam.xyz>
2024-02-03 21:45:48 -0500

    ui,api: delete unused endpoints and code
    Used knip which was pretty accurate:
    
    https://knip.dev/overview/getting-started
    
    ❯ npx knip
    
    Need to install the following packages:
      knip@4.3.0
    Ok to proceed? (y) y
    Analyzing workspace ....
    Unused files (5)
    src/api/ablyRetrieve.ts
    src/api/recipeList.ts
    src/api/searchClickCreate.ts
    src/pages/schedule/IconSettings.tsx
    src/time.ts
    Unused dependencies (4)
    @types/dotenv         package.json
    @types/fold-to-ascii  package.json
    dnd-core              package.json
    fold-to-ascii         package.json
    Unused devDependencies (2)
    @testing-library/jest-dom  package.json
    eslint-plugin-html         package.json
    Unlisted dependencies (13)
    react-aria                   src/components/Buttons.tsx
    instantsearch.js             src/components/CustomHighlight.tsx
    mdast-util-find-and-replace  src/components/Markdown.tsx
    react-aria                   src/components/NavRecipeSearch.tsx
    lodash/groupBy               src/pages/recipe-detail/Reactions.tsx
    instantsearch.js             src/pages/recipe-list/RecipeItem.tsx
    react-aria                   src/pages/schedule/CalendarDay.tsx
    react-aria                   src/pages/schedule/CalendarDayItem.tsx
    @algolia/client-search       src/pages/schedule/ScheduleRecipeModal.tsx
    react-aria                   src/pages/schedule/ScheduleRecipeModal.tsx
    @algolia/client-search       src/queries/searchRecipes.ts
    @algolia/client-search       src/queries/useAlgoliaClient.ts
    @algolia/transporter         src/queries/useAlgoliaClient.ts
    Unused exports (7)
    DragIcon          function  src/components/icons.tsx:1:17
    atLeast1          function  src/input.ts:6:17
    TimelineEvent     function  src/pages/recipe-detail/Notes.tsx:432:17
    Calendar          function  src/pages/schedule/Calendar.tsx:228:17
    ExportLink        function  src/pages/settings/Export.tsx:6:17
    pathDevStorybook  unknown   src/paths.tsx:30:14
    inviteURL         unknown   src/urls.ts:18:14
    Unused exports in namespaces (3)
    comparePositions  function  src/ordering.ts:18:17
    isValidPosition   function  src/ordering.ts:22:17
    DEBUG             unknown   src/settings.ts:3:14
    Unused exported types (3)
    IDays          type  src/pages/schedule/Calendar.tsx:35:13
    CookChecklist  type  src/queries/cookChecklistFetch.ts:8:13
    CookChecklist  type  src/queries/cookChecklistUpdate.ts:5:13
    Unused exported enum members (13)
    OUNCE        Unit  src/queries/shoppingListFetch.ts:10:3
    GRAM         Unit  src/queries/shoppingListFetch.ts:11:3
    KILOGRAM     Unit  src/queries/shoppingListFetch.ts:12:3
    TEASPOON     Unit  src/queries/shoppingListFetch.ts:13:3
    TABLESPOON   Unit  src/queries/shoppingListFetch.ts:14:3
    FLUID_OUNCE  Unit  src/queries/shoppingListFetch.ts:15:3
    CUP          Unit  src/queries/shoppingListFetch.ts:16:3
    PINT         Unit  src/queries/shoppingListFetch.ts:17:3
    QUART        Unit  src/queries/shoppingListFetch.ts:18:3
    GALLON       Unit  src/queries/shoppingListFetch.ts:19:3
    LITER        Unit  src/queries/shoppingListFetch.ts:20:3
    MILLILITER   Unit  src/queries/shoppingListFetch.ts:21:3
    POUND        Unit  src/queries/shoppingListFetch.ts:9:3
    Duplicate exports (1)
    Calendar, default  src/pages/schedule/Calendar.tsx

--- frontend/src/input.ts
+++ frontend/src/input.ts
@@ -2,8 +2,3 @@ export const inputAbs = (v: string) => {
   const c = v.replace(/[^\d]/g, "")
   return c.length === 0 ? 0 : parseInt(c, 10)
 }
-
-export function atLeast1(v: string) {
-  const abs = inputAbs(v)
-  return abs < 1 ? 1 : abs
-}
