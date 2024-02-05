baaa7d42348bf65365d696caee759651b05ade9e
Steve Dignam <steve@dignam.xyz>
2024-02-04 22:24:41 -0600

    infra: update logging for gunicorn and save configs (#1380)

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 set -eux
 
-PYTHONUNBUFFERED=1 exec /var/app/.venv/bin/gunicorn -w 3 -b 0.0.0.0:8000 recipeyak.api.base.wsgi --access-logfile - --error-logfile - --capture-output --enable-stdio-inheritance --access-logformat 'request="%(r)s" request_time=%(L)s remote_addr="%(h)s" request_id=%({X-Request-Id}i)s response_id=%({X-Response-Id}i)s method=%(m)s protocol=%(H)s status_code=%(s)s response_length=%(b)s referer="%(f)s" process_id=%(p)s user_agent="%(a)s"'
+# log formatting docs:
+# https://docs.gunicorn.org/en/stable/settings.html#access-log-format 
+
+PYTHONUNBUFFERED=1 exec /var/app/.venv/bin/gunicorn -w 3 -b 0.0.0.0:8000 recipeyak.api.base.wsgi --access-logfile - --error-logfile - --capture-output --enable-stdio-inheritance --access-logformat 'request="%(r)s" request_time=%(L)s remote_addr="%(h)s" request_id=%({X-Request-Id}i)s method=%(m)s protocol=%(H)s status_code=%(s)s response_length=%(b)s referer="%(f)s" process_id=%(p)s user_agent="%(a)s"'
