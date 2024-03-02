19cda2cba5ef91acff912df15cc41b0461da4673
Steve Dignam <steve@dignam.xyz>
2024-03-02 17:05:49 -0500

    ui: rework update scheduled recipe modal (#1403)
    <img width="481" alt="Screenshot 2024-03-02 at 4 48 47 PM" src="https://github.com/recipeyak/recipeyak/assets/7340772/6b37bfa9-f1d8-476d-b0fc-08bb380098c4">
    <img width="496" alt="Screenshot 2024-03-02 at 4 48 32 PM" src="https://github.com/recipeyak/recipeyak/assets/7340772/9ba193a8-87e0-4b89-885e-ec5bf59fdb3a">
    trying to make the buttons easier to press on mobile

--- frontend/src/queries/scheduledRecipeFindNextOpen.ts
+++
@@ -1,9 +0,0 @@
-import { useMutation } from "@tanstack/react-query"
-
-import { calendarNextOpen } from "@/api/calendarNextOpen"
-
-export function useScheduledRecipeFindNextOpen() {
-  return useMutation({
-    mutationFn: calendarNextOpen,
-  })
-}
