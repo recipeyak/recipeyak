722d9ae08e20714f1848183ea20b2aacb9f7bc6c
Steve Dignam <steve@dignam.xyz>
2024-05-04 20:04:02 -0400

    ui: link to git sha

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,14 @@
+import { GIT_SHA } from "@/settings"
+
 export const Footer = () => (
   <footer className="mt-auto flex items-center px-4 py-6 text-[14px]">
     <span className="font-semibold print:hidden">
       Recipe Yak •{" "}
-      <a href="https://github.com/recipeyak/recipeyak" className="font-medium">
-        src
+      <a
+        href={`https://github.com/recipeyak/recipeyak/commit/${GIT_SHA}`}
+        className="font-medium"
+      >
+        {GIT_SHA.slice(0, 7)}
       </a>
     </span>
   </footer>
