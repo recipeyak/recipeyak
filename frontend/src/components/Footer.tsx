281b248cb0cf6f98111b1ed56235500dd850f805
Christopher Dignam <chris@dignam.xyz>
2024-05-18 11:08:52 -0400

    make header sticky to fix hiding bottom of page
    I needed to use the order property to reposition the header

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,13 @@
+import { clx } from "@/classnames"
 import { GIT_SHA } from "@/settings"
 
-export const Footer = () => (
-  <footer className="mt-auto flex items-center px-4 py-6 text-[14px]">
+export const Footer = (props: { className?: string }) => (
+  <footer
+    className={clx(
+      "mt-auto flex items-center px-4 py-2 text-[14px]",
+      props.className,
+    )}
+  >
     <span className="font-semibold print:hidden">
       Recipe Yak •{" "}
       <a
