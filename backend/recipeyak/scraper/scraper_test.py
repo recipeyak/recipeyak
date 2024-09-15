7bfe836dde4ea1724197cb9c3d25db3d35e8d7e3
Steve Dignam <steve@dignam.xyz>
2024-09-15 17:42:23 -0400

    api: add test for scraping links from steps (#1504)
    I think recipe_scrapers handles this.

--- backend/recipeyak/scraper/scraper_test.py
+++ backend/recipeyak/scraper/scraper_test.py
@@ -19,5 +19,21 @@ def test_parse_recipe_tips_html_to_markdown(snapshot: SnapshotAssertion) -> None
         html=html,
         url="https://cooking.nytimes.com/recipes/1023609-chile-crisp-fettuccine-alfredo-with-spinach",
     )
+    assert result == snapshot()
 
+
+def test_parse_links_in_steps(snapshot: SnapshotAssertion) -> None:
+    """
+    Check that we convert links from html to markdown in steps.
+    """
+    html = (
+        Path(__file__).parent
+        / "test_data"
+        / "pressure-cooker-fast-and-easy-chicken-enchiladas-food-lab-recipe.html"
+    ).read_bytes()
+
+    result = _parse_recipe(
+        html=html,
+        url="https://www.seriouseats.com/pressure-cooker-fast-and-easy-chicken-enchiladas-food-lab-recipe",
+    )
     assert result == snapshot()
